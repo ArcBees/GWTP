65b82da7b911f6c163acb6c096d61479bd948cd0
philippe.beaudoin <none@none>
2010-03-30 17:52:56 +0000

    Incomplete - task 36: Test Presenter-Proxy exchanges http://code.google.com/p/gwt-platform/issues/detail?id=36
    Starter writing PresenterProxyIntegrationTest. It starts correctly but doesn't pass yet, we should first solve Issue 38:
    http://code.google.com/p/gwt-platform/issues/detail?id=38
    
    --HG--
    extra : convert_revision : svn%3A9b2172d4-2762-11df-83c2-19b91ddea5f5/trunk%4015
    
    
    Former-commit-id: 00fb22bace2f11a664ee7f355b8ec3132f1ec6f0

---
+++ gwttest/com/philbeaudoin/gwtp/mvp/client/PresenterProxyIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.philbeaudoin.gwtp.mvp.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+import com.philbeaudoin.gwtp.mvp.client.proxy.Proxy;
+import com.philbeaudoin.gwtp.mvp.rebind.ProxyCodeSplit;
+
+/**
+ * Integration test for {@link PresenterImpl} and 
+ * automatically-generated {@link Proxy} classes.
+ * <p />
+ * TODO: This doesn't work yet. See http://code.google.com/p/gwt-platform/issues/detail?id=38
+ * 
+ * @author Philippe Beaudoin
+ */
+public class PresenterProxyIntegrationTest extends GWTTestCase {
+
+  public static class MyPresenter extends PresenterImpl<View, MyPresenter.MyProxy> {
+
+    @ProxyCodeSplit
+    public interface MyProxy extends Proxy<MyPresenter> {}
+        
+    public MyPresenter() {
+      super((EventBus)GWT.create(EventBus.class), (View)GWT.create(View.class), (MyProxy)GWT.create(MyProxy.class));
+    }
+
+    @Override
+    protected void revealInParent() {
+      // TODO Auto-generated method stubs
+    }
+    
+  }
+  
+  public void test() {
+    GWT.create(MyPresenter.class);   
+  }
+
+  @Override
+  public String getModuleName() {
+    return "com.philbeaudoin.gwtp.mvp.Mvp";
+  }
+}
