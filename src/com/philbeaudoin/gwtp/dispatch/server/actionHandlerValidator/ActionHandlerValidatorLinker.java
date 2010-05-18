ccaa5c66f56641392dbfdf34785d6c27e2770429
Christian Goudreau <none@none>
2010-05-18 14:41:40 -0400

    Incomplete - task 63: Clean-up registries in dispatch http://code.google.com/p/gwt-platform/issues/detail?id=63
    Complete - task 80: Pass action as a parameter to ActionValidator.isValid
    http://code.google.com/p/gwt-platform/issues/detail?id=80
    
    Former-commit-id: 0fa9cf7c4944820e7479e9a3513732b8256495b4

--- src/com/philbeaudoin/gwtp/dispatch/server/actionHandlerValidator/ActionHandlerValidatorLinker.java
+++ src/com/philbeaudoin/gwtp/dispatch/server/actionHandlerValidator/ActionHandlerValidatorLinker.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2010 Gwt-Platform
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.philbeaudoin.gwtp.dispatch.server.actionHandlerValidator;
 
 import java.util.List;
@@ -8,11 +24,11 @@ import com.google.inject.Injector;
 import com.google.inject.TypeLiteral;
 import com.philbeaudoin.gwtp.dispatch.server.actionHandler.ActionHandler;
 import com.philbeaudoin.gwtp.dispatch.server.actionValidator.ActionValidator;
-import com.philbeaudoin.gwtp.dispatch.server.actionValidator.ActionValidatorRegistry;
 
 /**
- * This class links any registered {@link ActionValidator} instances with
- * the default {@link ActionValidatorRegistry}
+ * This class links any registered {@link ActionHandler} and
+ * {@link ActionValidator} instances with the default
+ * {@link ActionHandlerValidatorRegistry}
  * 
  * @author Christian Goudreau
  * 
@@ -32,9 +48,18 @@ public class ActionHandlerValidatorLinker {
               Class<? extends ActionValidator> actionValidatorClass = binding.getProvider().get().getActionHandlerValidatorClass().getActionValidatorClass();
               Class<? extends ActionHandler<?, ?>> handlerClass = binding.getProvider().get().getActionHandlerValidatorClass().getActionHandlerClass();
               
-              ActionHandlerValidatorInstance actionHandlerValidatorInstance = new ActionHandlerValidatorInstance(
-                  injector.getInstance(actionValidatorClass), 
-                  injector.getInstance(handlerClass));
+              ActionHandlerValidatorInstance actionHandlerValidatorInstance = null;
+              ActionValidator actionValidator = instanceRegistry.findActionValidator(actionValidatorClass);
+              
+              if (actionValidator == null) {
+                actionValidator =  injector.getInstance(actionValidatorClass);
+                
+                actionHandlerValidatorInstance = new ActionHandlerValidatorInstance(
+                    actionValidator, injector.getInstance(handlerClass));   
+              } else {
+                actionHandlerValidatorInstance = new ActionHandlerValidatorInstance(
+                    actionValidator, injector.getInstance(handlerClass));       
+              }
               
               instanceRegistry.addActionHandlerValidator(binding.getProvider().get().getActionClass(), actionHandlerValidatorInstance);
           }
