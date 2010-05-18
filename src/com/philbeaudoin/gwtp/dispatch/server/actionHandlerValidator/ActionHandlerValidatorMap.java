6563f285e64e823fa75437f1db963c7a87144cf7
Christian Goudreau <none@none>
2010-05-18 14:41:40 -0400

    Incomplete - task 63: Clean-up registries in dispatch http://code.google.com/p/gwt-platform/issues/detail?id=63
    Complete - task 80: Pass action as a parameter to ActionValidator.isValid
    http://code.google.com/p/gwt-platform/issues/detail?id=80
    
    Former-commit-id: 965a7fee52703ade3411b53e2b2154f44965fe43

--- src/com/philbeaudoin/gwtp/dispatch/server/actionHandlerValidator/ActionHandlerValidatorMap.java
+++ src/com/philbeaudoin/gwtp/dispatch/server/actionHandlerValidator/ActionHandlerValidatorMap.java
@@ -1,15 +1,32 @@
+/**
+ * Copyright 2010 Gwt-Platform
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.philbeaudoin.gwtp.dispatch.server.actionHandlerValidator;
 
 import com.philbeaudoin.gwtp.dispatch.shared.Action;
 import com.philbeaudoin.gwtp.dispatch.shared.Result;
 
 /**
- * This is the interface that define the map of {@link ActionHandlerValidatorInstance}.
+ * This is the interface that define the map of
+ * {@link ActionHandlerValidatorInstance}.
  * 
  * @param <A>
- *            Type of the associated {@link Action}
+ *          Type of the associated {@link Action}
  * @param <R>
- *            Type of the associated {@link Result}
+ *          Type of the associated {@link Result}
  * 
  * @author Christian Goudreau
  */
@@ -18,7 +35,7 @@ public interface ActionHandlerValidatorMap<A extends Action<R>, R extends Result
    * @return the {@link Action} class associated
    */
   public Class<A> getActionClass();
-  
+
   /**
    * @return the {@link ActionHandlerValidatorClass} class associated
    */
