0beacce0b920dbb4e47552fb46f6b944be63e662
Christian Goudreau <none@none>
2010-05-27 13:16:17 -0400

    Changed Philippe Beaudoin to Gwt-Platform and add some missing licence header.
    Former-commit-id: af106ad3f11c837e0d84f89ccd9dafb2c7881ee0

--- src/com/philbeaudoin/gwtp/dispatch/shared/Result.java
+++ src/com/philbeaudoin/gwtp/dispatch/shared/Result.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010 Philippe Beaudoin
+ * Copyright 2010 Gwt-Platform
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
