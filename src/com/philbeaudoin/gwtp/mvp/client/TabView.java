3a8886a175b579535f070b07d8650398686630f1
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-05-28 00:47:31 -0700

    Complete - task 83: Make a TabView interface grouping View and TabPanel http://code.google.com/p/gwt-platform/issues/detail?id=83
    Former-commit-id: a066cc04410d5a603d052e650d93993081946f6a

---
+++ src/com/philbeaudoin/gwtp/mvp/client/TabView.java
@@ -0,0 +1,10 @@
+package com.philbeaudoin.gwtp.mvp.client;
+
+/**
+ * The interface of a {@link View} that is also a {@link TabPanel}.
+ * 
+ * @author Philippe Beaudoin
+ */
+public interface TabView extends View, TabPanel {
+
+}
