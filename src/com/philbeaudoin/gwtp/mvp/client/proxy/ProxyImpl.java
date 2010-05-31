3f1aff7eadd27152b23887fddb916e38e774f9de
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-05-30 18:55:22 -0700

    Incomplete - task 84: Support hierarchical places http://code.google.com/p/gwt-platform/issues/detail?id=84
    Not working yet, but getting there. This version is functional (without hierarchical places) but it has some breaking changes described in PortingV1.

--- src/com/philbeaudoin/gwtp/mvp/client/proxy/ProxyImpl.java
+++ src/com/philbeaudoin/gwtp/mvp/client/proxy/ProxyImpl.java
@@ -56,24 +56,6 @@ implements Proxy<P> {
   public void getPresenter( AsyncCallback<P> callback ) {
     presenter.get(callback);
   }
-
-  /**
-   * By default, proxys can't reveal their presenter. Only place
-   * proxies, such as {@link ProxyPlaceAbstract} can. 
-   */
-  @Override
-  public void reveal() {
-    assert false : "Unrevealable proxy.";
-  }
-
-  /**
-   * By default, proxys can't reveal their presenter. Only place
-   * proxies, such as {@link ProxyPlaceAbstract} can. 
-   */
-  @Override
-  public void reveal(PlaceRequest request) {
-    assert false : "Unrevealable proxy.";
-  }
   
   @Override
   public void onPresenterChanged( Presenter presenter ) {
