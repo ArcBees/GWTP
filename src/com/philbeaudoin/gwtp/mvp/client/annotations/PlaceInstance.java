46a6fed801209d4e876420fdf394631d4d1feec4
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-06-10 22:13:27 -0700

    Complete - task 101: Proposal: replace PlaceInstance by a PlaceProvider interface and @PlaceGenerator annotation http://code.google.com/p/gwt-platform/issues/detail?id=101
    Updated documentation, maked @PlaceInstance as deprecated, updated error message, added support for a @DefaultPlaceProvider. This still needs to be thoroughly tested.
    
    --HG--
    rename : src/com/philbeaudoin/gwtp/mvp/client/PlaceFactory.java => src/com/philbeaudoin/gwtp/mvp/client/proxy/PlaceFactory.java

--- src/com/philbeaudoin/gwtp/mvp/client/annotations/PlaceInstance.java
+++ src/com/philbeaudoin/gwtp/mvp/client/annotations/PlaceInstance.java
@@ -19,14 +19,15 @@ package com.philbeaudoin.gwtp.mvp.client.annotations;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-import com.google.gwt.inject.client.Ginjector;
 
 /**
+ * <b>Note:</b> This annotation has been deprecated. Use {@link PlaceProvider} instead.
+ * <p />
  * Annotation used to specify the code to call when creating a 
  * new place. Usually a simple call to {@code new}. Make sure
  * class names are fully qualified. Also, you have access
  * to the variable {@code nameToken} (a String) and 
- * {@code ginjector} (your specific {@link Ginjector}-derived
+ * {@code ginjector} (your specific {@link com.google.gwt.inject.client.Ginjector}-derived
  * class). For example:
  * <pre>
  * {@code @}NewPlaceCode( "new com.project.client.AdminSecurePlace(nameToken, ginjector.getCurrentUser())" )
@@ -34,6 +35,7 @@ import com.google.gwt.inject.client.Ginjector;
  * 
  * @author Philippe Beaudoin
  */
+@Deprecated
 @Target(ElementType.TYPE)
 public @interface PlaceInstance {
   String value();
