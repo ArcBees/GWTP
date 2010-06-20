8b13b037430fe9d280360840b398829a6b927681
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-06-20 15:46:35 -0700

    Complete - task 110: http://code.google.com/p/gwt-platform/issues/detail?id=110
    Renamed PlaceRequestEvent to PlaceRequestInternalEvent. Same with Handler. Made these classes package-private so they shouldn't be fired by 3rd party. Use the PlaceManager methods instead, or see the PortingV1 wiki.
    
    --HG--
    rename : src/com/philbeaudoin/gwtp/mvp/client/proxy/PlaceRequestEvent.java => src/com/philbeaudoin/gwtp/mvp/client/proxy/PlaceRequestInternalEvent.java
    rename : src/com/philbeaudoin/gwtp/mvp/client/proxy/PlaceRequestHandler.java => src/com/philbeaudoin/gwtp/mvp/client/proxy/PlaceRequestInternalHandler.java
    
    
    Former-commit-id: 14a4416a5b8473c2a0555414b0e2e156a706e68d

--- src/com/philbeaudoin/gwtp/mvp/client/proxy/PlaceRequestInternalHandler.java
+++ src/com/philbeaudoin/gwtp/mvp/client/proxy/PlaceRequestInternalHandler.java
@@ -18,7 +18,12 @@ package com.philbeaudoin.gwtp.mvp.client.proxy;
 
 import com.google.gwt.event.shared.EventHandler;
 
-public interface PlaceRequestHandler extends EventHandler {
+/**
+ * Proxy should implement this class.
+ * 
+ * @author Philippe Beaudoin
+ */
+interface PlaceRequestInternalHandler extends EventHandler {
   /**
    * Called when something has requested a new place. Should be implemented by
    * instances which can show the place.
@@ -26,5 +31,5 @@ public interface PlaceRequestHandler extends EventHandler {
    * @param event
    *            The event.
    */
-  void onPlaceRequest( PlaceRequestEvent event );
+  void onPlaceRequest( PlaceRequestInternalEvent event );
 }
