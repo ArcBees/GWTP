1eb806c960aebd5befad7223ea6d7c04a6c8c724
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-07-18 00:12:57 -0700

    Complete - task 71: Allow binding presenter via an interface http://code.google.com/p/gwt-platform/issues/detail?id=71
    Former-commit-id: f6d34c72301544972a32a23b91dea62c9986de6d

--- src/com/philbeaudoin/gwtp/mvp/client/gin/AbstractPresenterModule.java
+++ src/com/philbeaudoin/gwtp/mvp/client/gin/AbstractPresenterModule.java
@@ -33,17 +33,36 @@ public abstract class AbstractPresenterModule extends AbstractGinModule {
   /**
    * Convenience method for binding a non-singleton {@link PresenterWidget} with its {@link View}.
    *
-   * @param <P>         The {@link PresenterWidget} type.
-   * @param <V>         The {@link View} type.
-   * @param presenter   The {@link PresenterWidget} (NOT a singleton).
+   * @param <P>         The {@link PresenterWidget} interface type.
+   * @param <V>         The {@link View} interface type.
+   * @param presenter   The {@link PresenterWidget} interface.
+   * @param presenterImpl   The {@link PresenterWidget} implementation (NOT a singleton).
    * @param view     The {@link View} interface.
    * @param viewImpl The {@link View} implementation (NOT a singleton).
    */
   protected <P extends PresenterWidget, V extends View> void bindPresenterWidget( 
-      Class<P> presenter, 
+      Class<P> presenter,
+      Class<? extends P> presenterImpl,
       Class<V> view,
       Class<? extends V> viewImpl ) {
-    bind( presenter );
+    bind( presenter ).to( presenterImpl );
+    bind( view ).to( viewImpl );
+  }
+
+  /**
+   * Convenience method for binding a non-singleton {@link PresenterWidget} with its {@link View}.
+   *
+   * @param <P>         The {@link PresenterWidget} class type.
+   * @param <V>         The {@link View} interface type.
+   * @param presenterImpl   The {@link PresenterWidget} implementation (NOT a singleton).
+   * @param view     The {@link View} interface.
+   * @param viewImpl The {@link View} implementation (NOT a singleton).
+   */
+  protected <P extends PresenterWidget, V extends View> void bindPresenterWidget( 
+      Class<P> presenterImpl, 
+      Class<V> view,
+      Class<? extends V> viewImpl ) {
+    bind( presenterImpl );
     bind( view ).to( viewImpl );
   }
 
@@ -53,7 +72,7 @@ public abstract class AbstractPresenterModule extends AbstractGinModule {
    * a factory.
    *
    * @param <P> The type of the {@link PresenterWidget} factory.
-   * @param <V>  The type of the {@link View} factory.
+   * @param <V> The type of the {@link View} factory.
    * @param presenterFactory The interface to the {@link PresenterWidget} factory.
    * @param presenterFactoryImpl The implementation of the {@link PresenterWidget} factory.
    * @param viewFactory The interface to the {@link View} factory.
@@ -78,17 +97,43 @@ public abstract class AbstractPresenterModule extends AbstractGinModule {
    * presenter widget as content in its containing presenter {@link PresenterImpl#onReveal} and to
    * remove it in the {@link PresenterImpl#onHide}.
    *
-   * @param <P>         The {@link PresenterWidget} type.
-   * @param <V>         The {@link View} type.
-   * @param presenter   The {@link PresenterWidget} (a singleton).
+   * @param <P>         The {@link PresenterWidget} interface type.
+   * @param <V>         The {@link View} interface type.
+   * @param presenter   The {@link PresenterWidget} interface (a singleton).
+   * @param presenterImpl   The {@link PresenterWidget} implementation (a singleton).
    * @param view     The {@link View} interface.
    * @param viewImpl The {@link View} implementation (a singleton).
    */
   protected <P extends PresenterWidget, V extends View> void bindSingletonPresenterWidget( 
-      Class<P> presenter, 
+      Class<P> presenter,
+      Class<? extends P> presenterImpl,
       Class<V> view,
       Class<? extends V> viewImpl ) {
-    bind( presenter ).in(Singleton.class);
+    bind( presenter ).to( presenterImpl ).in(Singleton.class);
+    bind( view ).to( viewImpl ).in(Singleton.class);
+  }
+  
+  /**
+   * Convenience method for binding a singleton {@link PresenterWidget} with its {@link View}.
+   * <p />
+   * <b>Important!</b> If you want to use the same {@link PresenterWidget} in many different places,
+   * you should consider making it non-singleton with {@link #bindPresenterWidget}. It is possible
+   * to use the same singleton {@link PresenterWidget} in different presenters, as long as these
+   * are not simultaneously visible. Also, if you do this, you must make sure to set the singleton
+   * presenter widget as content in its containing presenter {@link PresenterImpl#onReveal} and to
+   * remove it in the {@link PresenterImpl#onHide}.
+   *
+   * @param <P>         The {@link PresenterWidget} class type.
+   * @param <V>         The {@link View} interface type.
+   * @param presenterImpl   The {@link PresenterWidget} implementation (a singleton).
+   * @param view     The {@link View} interface.
+   * @param viewImpl The {@link View} implementation (a singleton).
+   */
+  protected <P extends PresenterWidget, V extends View> void bindSingletonPresenterWidget( 
+      Class<P> presenterImpl, 
+      Class<V> view,
+      Class<? extends V> viewImpl ) {
+    bind( presenterImpl ).in(Singleton.class);
     bind( view ).to( viewImpl ).in(Singleton.class);
   }
 
@@ -96,23 +141,48 @@ public abstract class AbstractPresenterModule extends AbstractGinModule {
    * Convenience method for binding a singleton presenter with its view and 
    * its proxy, when using automatically generated proxy classes.
    *
-   * @param <P>         The {@link Presenter} type.
-   * @param <V>         The {@link View} type.
+   * @param <P>         The {@link Presenter} interface type.
+   * @param <V>         The {@link View} interface type.
    * @param <Proxy_>    The {@link Proxy} type.
-   * @param presenter   The {@link Presenter} (a singleton).
+   * @param presenter The {@link Presenter} interface.
+   * @param presenterImpl   The {@link Presenter} implementation (a singleton).
    * @param view     The {@link View} interface.
    * @param viewImpl The {@link View} implementation (a singleton).
    * @param proxy       The {@link Proxy} interface, which will lead to an automatically 
    *                    generated proxy classes.
-   * 
-   * @see #bindPresenter(Class, Class, Class, Class, Class)
    */
   protected <P extends Presenter, V extends View, Proxy_ extends Proxy<P>> void bindPresenter( 
-      Class<P> presenter, 
+      Class<P> presenter,
+      Class<? extends P> presenterImpl,
       Class<V> view,
       Class<? extends V> viewImpl,
       Class<Proxy_> proxy ) {
-    bind( presenter ).in( Singleton.class );
+    bind( presenterImpl ).in( Singleton.class );
+    bind( viewImpl ).in( Singleton.class );
+    bind( proxy ).asEagerSingleton();
+    bind( presenter ).to( presenterImpl );
+    bind( view ).to( viewImpl );
+  }
+  
+  /**
+   * Convenience method for binding a singleton presenter with its view and 
+   * its proxy, when using automatically generated proxy classes.
+   *
+   * @param <P>         The {@link Presenter} class type.
+   * @param <V>         The {@link View} interface type.
+   * @param <Proxy_>    The {@link Proxy} type.
+   * @param presenterImpl   The {@link Presenter} implementation (a singleton).
+   * @param view     The {@link View} interface.
+   * @param viewImpl The {@link View} implementation (a singleton).
+   * @param proxy       The {@link Proxy} interface, which will lead to an automatically 
+   *                    generated proxy classes.
+   */
+  protected <P extends Presenter, V extends View, Proxy_ extends Proxy<P>> void bindPresenter( 
+      Class<P> presenterImpl,
+      Class<V> view,
+      Class<? extends V> viewImpl,
+      Class<Proxy_> proxy ) {
+    bind( presenterImpl ).in( Singleton.class );
     bind( viewImpl ).in( Singleton.class );
     bind( proxy ).asEagerSingleton();
     bind( view ).to( viewImpl );
@@ -122,28 +192,81 @@ public abstract class AbstractPresenterModule extends AbstractGinModule {
    * Convenience method for binding a singleton presenter with its view and 
    * its proxy, when using custom-made proxy classes.
    *
-   * @param <P>         The {@link Presenter} type.
-   * @param <V>         The {@link View} type.
+   * @param <P>         The {@link Presenter} interface type.
+   * @param <V>         The {@link View} interface type.
    * @param <Proxy_>    The {@link Proxy} type.
-   * @param presenter   The {@link Presenter} (a singleton).
+   * @param presenter   The {@link Presenter} interface.
+   * @param presenterImpl   The {@link Presenter} implementation (a singleton).
    * @param view     The {@link View} interface.
    * @param viewImpl The {@link View} implementation (a singleton).
    * @param proxy       The {@link Proxy} interface.
    * @param proxyImpl   The {@link Proxy} implementation (a singleton).
-   * 
-   * @see #bindPresenter(Class, Class, Class, Class)
    */
-  protected <P extends Presenter, V extends View, Proxy_ extends Proxy<P>> void bindPresenter( 
+  protected <P extends Presenter, V extends View, Proxy_ extends Proxy<P>> void bindPresenterCustomProxy( 
       Class<P> presenter, 
+      Class<? extends P> presenterImpl, 
       Class<V> view,
       Class<? extends V> viewImpl,
       Class<Proxy_> proxy,
       Class<? extends Proxy_> proxyImpl ) {
-    bind( presenter ).in( Singleton.class );
+    bind( presenterImpl ).in( Singleton.class );
+    bind( viewImpl ).in( Singleton.class );
+    bind( proxyImpl ).asEagerSingleton();
+    bind( presenter ).to( presenterImpl );
+    bind( view ).to( viewImpl );
+    bind( proxy ).to( proxyImpl );
+  }
+  
+  /**
+   * Convenience method for binding a singleton presenter with its view and 
+   * its proxy, when using custom-made proxy classes.
+   *
+   * @param <P>         The {@link Presenter} class type.
+   * @param <V>         The {@link View} interface type.
+   * @param <Proxy_>    The {@link Proxy} type.
+   * @param presenterImpl   The {@link Presenter} implementation (a singleton).
+   * @param view     The {@link View} interface.
+   * @param viewImpl The {@link View} implementation (a singleton).
+   * @param proxy       The {@link Proxy} interface.
+   * @param proxyImpl   The {@link Proxy} implementation (a singleton).
+   */
+  protected <P extends Presenter, V extends View, Proxy_ extends Proxy<P>> void bindPresenterCustomProxy( 
+      Class<P> presenterImpl, 
+      Class<V> view,
+      Class<? extends V> viewImpl,
+      Class<Proxy_> proxy,
+      Class<? extends Proxy_> proxyImpl ) {
+    bind( presenterImpl ).in( Singleton.class );
     bind( viewImpl ).in( Singleton.class );
     bind( proxyImpl ).asEagerSingleton();
     bind( view ).to( viewImpl );
     bind( proxy ).to( proxyImpl );
+  }
+
+  /**
+   * Convenience method for binding a singleton presenter with
+   * its proxy, when using automatically generated proxy classes and non-singleton views.
+   * <p />
+   * <b>Important!</b> This is only be meant to be used by presenters associated 
+   * with non-singleton views, for example when the same view class is reused with 
+   * many presenters. As such, you will need to also use the {@link #bindSharedView} method.
+   * If the view class is use only by one presenter, you should consider using
+   * {@link #bindPresenter(Class, Class, Class, Class, Class)} instead.
+   *
+   * @param <P>         The {@link Presenter} interface type.
+   * @param <Proxy_>    The {@link Proxy} type.
+   * @param presenterImpl   The {@link Presenter} interface.
+   * @param presenterImpl   The {@link Presenter} implementation (a singleton).
+   * @param proxy       The {@link Proxy} interface, which will lead to an automatically 
+   *                    generated proxy classes.
+   */
+  protected <P extends Presenter, Proxy_ extends Proxy<P>> void bindPresenter( 
+      Class<P> presenter,
+      Class<? extends P> presenterImpl,
+      Class<Proxy_> proxy ) {
+    bind( presenterImpl ).in( Singleton.class );
+    bind( proxy ).asEagerSingleton();
+    bind( presenter ).to( presenterImpl );
   }
   
   /**
@@ -156,18 +279,16 @@ public abstract class AbstractPresenterModule extends AbstractGinModule {
    * If the view class is use only by one presenter, you should consider using
    * {@link #bindPresenter(Class, Class, Class, Class)} instead.
    *
-   * @param <P>         The {@link Presenter} type.
+   * @param <P>         The {@link Presenter} class type.
    * @param <Proxy_>    The {@link Proxy} type.
-   * @param presenter   The {@link Presenter} (a singleton).
+   * @param presenterImpl   The {@link Presenter} implementation (a singleton).
    * @param proxy       The {@link Proxy} interface, which will lead to an automatically 
    *                    generated proxy classes.
-   * 
-   * @see #bindPresenter(Class, Class, Class)
    */
   protected <P extends Presenter, Proxy_ extends Proxy<P>> void bindPresenter( 
-      Class<P> presenter,
+      Class<P> presenterImpl,
       Class<Proxy_> proxy ) {
-    bind( presenter ).in( Singleton.class );
+    bind( presenterImpl ).in( Singleton.class );
     bind( proxy ).asEagerSingleton();
   }
 
@@ -179,21 +300,47 @@ public abstract class AbstractPresenterModule extends AbstractGinModule {
    * with non-singleton views, for example when the same view class is reused with 
    * many presenters. As such, you will need to also use the {@link #bindSharedView} method.
    * If the view class is use only by one presenter, you should consider using
-   * {@link #bindPresenter(Class, Class, Class, Class, Class)} instead.
+   * {@link #bindPresenterCustomProxy(Class, Class, Class, Class, Class, Class)} instead.
    *
-   * @param <P>         The {@link Presenter} type.
+   * @param <P>         The {@link Presenter} interface type.
    * @param <Proxy_>    The {@link Proxy} type.
-   * @param presenter   The {@link Presenter} (a singleton).
+   * @param presenter   The {@link Presenter} interface.
+   * @param presenterImpl   The {@link Presenter} implementation (a singleton).
    * @param proxy       The {@link Proxy} interface.
    * @param proxyImpl   The {@link Proxy} implementation (a singleton).
-   * 
-   * @see #bindPresenter(Class, Class)
    */
-  protected <P extends Presenter,Proxy_ extends Proxy<P>> void bindPresenter( 
+  protected <P extends Presenter,Proxy_ extends Proxy<P>> void bindPresenterCustomProxy( 
       Class<P> presenter, 
+      Class<? extends P> presenterImpl, 
       Class<Proxy_> proxy,
       Class<? extends Proxy_> proxyImpl ) {
-    bind( presenter ).in( Singleton.class );
+    bind( presenterImpl ).in( Singleton.class );
+    bind( proxyImpl ).asEagerSingleton();
+    bind( presenter ).to( presenterImpl );
+    bind( proxy ).to( proxyImpl );
+  }
+  
+  /**
+   * Convenience method for binding a singleton presenter with 
+   * its proxy, when using custom-made proxy classes and non-singleton views.
+   * <p />
+   * <b>Important!</b> This is only be meant to be used by presenters associated 
+   * with non-singleton views, for example when the same view class is reused with 
+   * many presenters. As such, you will need to also use the {@link #bindSharedView} method.
+   * If the view class is used only by one presenter, you should consider using
+   * {@link #bindPresenterCustomProxy(Class, Class, Class, Class, Class)} instead.
+   *
+   * @param <P>         The {@link Presenter} class type.
+   * @param <Proxy_>    The {@link Proxy} type.
+   * @param presenterImpl   The {@link Presenter} implementation (a singleton).
+   * @param proxy       The {@link Proxy} interface.
+   * @param proxyImpl   The {@link Proxy} implementation (a singleton).
+   */
+  protected <P extends Presenter,Proxy_ extends Proxy<P>> void bindPresenterCustomProxy( 
+      Class<P> presenterImpl, 
+      Class<Proxy_> proxy,
+      Class<? extends Proxy_> proxyImpl ) {
+    bind( presenterImpl ).in( Singleton.class );
     bind( proxyImpl ).asEagerSingleton();
     bind( proxy ).to( proxyImpl );
   }
@@ -204,11 +351,13 @@ public abstract class AbstractPresenterModule extends AbstractGinModule {
    * <b>Important!</b> This is only be meant to be used for presenter associated 
    * with non-singleton views, for example when the same view class is reused with 
    * many presenters. As such, you will use this method with
-   * {@link #bindPresenter(Class, Class)} or {@link #bindPresenter(Class, Class, Class)}.
+   * {@link #bindPresenter(Class, Class)}, {@link #bindPresenter(Class, Class, Class)},
+   * {@link #bindPresenterCustomProxy(Class, Class, Class)}, or
+   * {@link #bindPresenterCustomProxy(Class, Class, Class, Class)},
    * If the view class is use only by one presenter, you should consider using
    * {@link #bindPresenter(Class, Class, Class, Class, Class)} instead.
    *
-   * @param <V>      The {@link View} type.
+   * @param <V>      The {@link View} interface type.
    * @param view     The {@link View} interface.
    * @param viewImpl The {@link View} implementation (not a singleton).
    */
