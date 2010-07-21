92b91da4281f0fbfa4cf5b96b6bc71b15b230d4a
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-07-20 23:32:36 -0700

    Complete - task 116: Add option to remove PresenterWidgetImpl from its parent http://code.google.com/p/gwt-platform/issues/detail?id=116

--- src/com/philbeaudoin/gwtp/mvp/client/View.java
+++ src/com/philbeaudoin/gwtp/mvp/client/View.java
@@ -29,8 +29,15 @@ public interface View {
   /**
    * Requests the view to set content within a specific slot,
    * clearing anything that was already contained there.
+   * <p />
+   * Override the default implementation and manage all the slots
+   * of your view into which content can be placed. 
    * If the view doesn't know about this slot, it can silently 
-   * ignore the request.
+   * ignore the request. When {@code null} is passed, your implementation 
+   * should clear the slot.
+   * <p />
+   * Used by {@link PresenterWidgetImpl#setContent(Object, PresenterWidget)} and
+   * {@link PresenterWidgetImpl#clearContent(Object)}.
    * 
    * @param slot An opaque object indicating the slot to add into.
    * @param content The content to add, a {@link Widget}. Pass {@code null} to clear the slot entirely.
@@ -39,14 +46,36 @@ public interface View {
   
   /**
    * Requests the view to add content within a specific slot.
+   * <p />
+   * Override the default implementation and manage all the slots
+   * of your view into which content can be added. You should
+   * also consider over overriding {@link #removeContent(Object, Widget)}.
    * If the view doesn't know about this slot, it can silently 
-   * ignore the request .
+   * ignore the request.
+   * <p />
+   * Used by {@link PresenterWidgetImpl#addContent(Object, PresenterWidget)}.
    * 
    * @param slot An opaque object indicating the slot to add into.
    * @param content The content to add, a {@link Widget}.
    */
   public void addContent( Object slot, Widget content );
   
+  /**
+   * Requests the view to remove content from a specific slot.
+   * <p />
+   * Override the default implementation and manage all the slots
+   * of your view into which content can be added and removed. You should
+   * also override {@link #addContent(Object, Widget)}.
+   * If the view doesn't know about this slot, it can silently 
+   * ignore the request.
+   * <p />
+   * Used by {@link PresenterWidgetImpl#removeContent(Object, PresenterWidget)}.
+   * 
+   * @param slot An opaque object indicating the slot to remove from.
+   * @param content The content to remove, a {@link Widget}.
+   */
+  public void removeContent( Object slot, Widget content );
+
   /**
    * Retrieves this view as a {@link Widget} so that it can be inserted within the DOM.
    * 
