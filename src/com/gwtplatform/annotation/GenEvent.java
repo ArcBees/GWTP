98ace796a1b1c31419c5cad1d89e3d21a78394f6
brendan <none@none>
2010-07-22 21:37:05 +1200

    final changes required to work with my app.
    Former-commit-id: bfb530b390dcfd48bb5e5491ec5263bd9e73d9e7

--- src/com/gwtplatform/annotation/GenEvent.java
+++ src/com/gwtplatform/annotation/GenEvent.java
@@ -22,6 +22,38 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * If you type:
+ * 
+ * <pre>
+ * <code> 
+ * {@literal}@GenEvent
+ * public class FooChanged {
+ *   Foo foo;
+ *   boolean originator;
+ * }
+ * </code>
+ * </pre>
+ * 
+ * gwt-platform will generate two classes, FooChangedEvent and
+ * FooChangedHandler.
+ * <p/>
+ * FooChangedEvent will have fields, getters, and a constructor for foo and
+ * originator, plus static getType(), instance dispatch, etc., for it to
+ * function correctly as a GwtEvent.
+ * <p/>
+ * FooChangedHandler will be an interface with a onFooChanged method that takes
+ * a FooChangedEvent parameter.
+ * 
+ * Notes:
+ * 
+ * There is no naming requirement for your class name. It will be appended with
+ * Event and Handler.
+ * 
+ * @author Brendan Doherty (All original code, concept attributed to Stephen
+ *         Haberman)
+ */
+
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.SOURCE)
 @Inherited
