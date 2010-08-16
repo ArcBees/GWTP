9576ed45facb78942497543c199115237c65a693
Christian Goudreau <none@none>
2010-08-16 18:09:07 -0400

    Incomplete - task 165: Change packages names to lower case. http://code.google.com/p/gwt-platform/issues/detail?id=165
    First step.
    
    Also committed upload.py.
    
    --HG--
    branch : Issue 165
    rename : src/com/gwtplatform/dispatch/server/actionHandler/AbstractActionHandler.java => src/com/gwtplatform/dispatch/server/actionhandlerr/AbstractActionHandler.java
    rename : src/com/gwtplatform/dispatch/server/actionHandler/ActionHandler.java => src/com/gwtplatform/dispatch/server/actionhandlerr/ActionHandler.java
    rename : src/com/gwtplatform/dispatch/server/actionHandler/ActionResult.java => src/com/gwtplatform/dispatch/server/actionhandlerr/ActionResult.java
    rename : src/com/gwtplatform/dispatch/server/actionHandler/BatchActionHandler.java => src/com/gwtplatform/dispatch/server/actionhandlerr/BatchActionHandler.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/ActionHandlerValidatorClass.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/ActionHandlerValidatorClass.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/ActionHandlerValidatorInstance.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/ActionHandlerValidatorInstance.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/ActionHandlerValidatorLinker.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/ActionHandlerValidatorLinker.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/ActionHandlerValidatorMap.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/ActionHandlerValidatorMap.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/ActionHandlerValidatorRegistry.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/ActionHandlerValidatorRegistry.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/EagerActionHandlerValidatorRegistry.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/EagerActionHandlerValidatorRegistry.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/EagerActionHandlerValidatorRegistryImpl.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/EagerActionHandlerValidatorRegistryImpl.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/LazyActionHandlerValidatorRegistry.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/LazyActionHandlerValidatorRegistry.java
    rename : src/com/gwtplatform/dispatch/server/actionHandlerValidator/LazyActionHandlerValidatorRegistryImpl.java => src/com/gwtplatform/dispatch/server/actionhandlervalidatorr/LazyActionHandlerValidatorRegistryImpl.java
    rename : src/com/gwtplatform/dispatch/server/actionValidator/ActionValidator.java => src/com/gwtplatform/dispatch/server/actionvalidatorr/ActionValidator.java
    rename : src/com/gwtplatform/dispatch/server/actionValidator/DefaultActionValidator.java => src/com/gwtplatform/dispatch/server/actionvalidatorr/DefaultActionValidator.java

--- src/com/gwtplatform/dispatch/server/actionhandlerr/AbstractActionHandler.java
+++ src/com/gwtplatform/dispatch/server/actionhandlerr/AbstractActionHandler.java
@@ -14,7 +14,7 @@
  * the License.
  */
 
-package com.gwtplatform.dispatch.server.actionHandler;
+package com.gwtplatform.dispatch.server.actionhandlerr;
 
 import com.gwtplatform.dispatch.shared.Action;
 import com.gwtplatform.dispatch.shared.Result;
--- src/com/gwtplatform/dispatch/server/actionhandlerr/BatchActionHandler.java
+++ src/com/gwtplatform/dispatch/server/actionhandlerr/BatchActionHandler.java
@@ -14,7 +14,7 @@
  * the License.
  */
 
-package com.gwtplatform.dispatch.server.actionHandler;
+package com.gwtplatform.dispatch.server.actionhandlerr;
 
 import com.gwtplatform.dispatch.server.ExecutionContext;
 import com.gwtplatform.dispatch.shared.Action;
