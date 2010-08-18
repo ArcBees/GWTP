18dcea8e01d62df5d140cd2c6f3260857b895b50
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-08-18 13:02:31 -0700

    Added failing unit tests when injecting mock PresenterWidget.
    --HG--
    branch : Issue 165

---
+++ test/com/gwtplatform/externaltest/RealInjectionTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.externaltest;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Names;
+
+import com.gwtplatform.mvp.client.DefaultEventBus;
+import com.gwtplatform.mvp.client.EventBus;
+import com.gwtplatform.mvp.client.PresenterWidget;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.testing.GuiceMockitoJUnitRunner;
+import com.gwtplatform.testing.TestModule;
+import com.gwtplatform.testing.TestScope;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Test behaviour when a real {@link PresenterWidget} is injected.
+ * 
+ * @author Philippe Beaudoin
+ */
+@RunWith(GuiceMockitoJUnitRunner.class)
+public class RealInjectionTest {
+
+  // Guice environment
+  /**
+   * @author Philippe Beaudoin
+   */
+  public static class Env extends TestModule {
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void configure() {
+      bind(new TypeLiteral<PresenterWidget<View>>() { })
+        .annotatedWith(Names.named("Sub"))
+        .to((Class<? extends PresenterWidget<View>>) SubPresenterWidget.class)
+        .in(TestScope.SINGLETON);
+      // TODO Provide methods for binding presenters, views and proxy
+      bind(EventBus.class).to(DefaultEventBus.class).in(TestScope.SINGLETON);
+      bindMock(MainPresenter.MyView.class).in(TestScope.SINGLETON);
+      bindMock(MainPresenter.MyProxy.class).in(TestScope.SINGLETON);
+      bindMock(SubPresenterWidget.MyView.class).in(TestScope.SINGLETON);
+    }
+  }
+
+  // Providers to use Guice injection
+  @Inject
+  Provider<MainPresenter> mainPresenterProvider;
+
+  @Test
+  public void settingSubPresenterShouldNotCrash() {
+    MainPresenter mainPresenter = mainPresenterProvider.get();
+
+    // When
+    mainPresenter.setSubPresenter();
+
+    // Then nothing should crash 
+  }
+}
