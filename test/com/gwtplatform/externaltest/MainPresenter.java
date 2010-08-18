57d1719a13c5dca4658d0b4f5a695e3a4fcf4223
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-08-18 13:02:31 -0700

    Added failing unit tests when injecting mock PresenterWidget.
    --HG--
    branch : Issue 165

---
+++ test/com/gwtplatform/externaltest/MainPresenter.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.externaltest;
+
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+
+import com.gwtplatform.mvp.client.EventBus;
+import com.gwtplatform.mvp.client.PresenterImpl;
+import com.gwtplatform.mvp.client.PresenterWidget;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.ContentSlot;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.proxy.Proxy;
+import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
+import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
+
+/**
+ * This is the test presenter.
+ * 
+ * @author Philippe Beaudoin
+ */
+@Singleton
+public class MainPresenter extends PresenterImpl<MainPresenter.MyView, MainPresenter.MyProxy> {
+
+  @ContentSlot
+  public static final Type<RevealContentHandler<?>> TYPE_SetMainContent = new Type<RevealContentHandler<?>>();
+  
+  /**
+   * Presenter's view.
+   */
+  public interface MyView extends View {    
+  }
+  
+  /**
+   * Presenter's proxy.
+   */
+  @ProxyStandard
+  public interface MyProxy extends Proxy<MainPresenter> {    
+  }
+
+  private PresenterWidget<?> subPresenter;
+  
+  @Inject
+  public MainPresenter(final EventBus eventBus, final MyView view, 
+      final MyProxy proxy, @Named("Sub") PresenterWidget<View> subPresenter) {
+    super(eventBus, view, proxy);
+    this.subPresenter = subPresenter;
+  }
+
+  public void setSubPresenter() {
+    setContent(TYPE_SetMainContent, subPresenter);
+  }
+  
+  @Override
+  protected void revealInParent() {
+    RevealRootContentEvent.fire(this, this);
+  }
+}
+
