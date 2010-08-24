f5eca237cb9d9f9e8b86a5a246030ef066396ad7
Christian Goudreau <none@none>
2010-08-24 10:00:16 -0400

    Made slot related methods final and renamed notifyReveal and notifyHide to reveal and hide.
    --HG--
    branch : Issue 165
    
    
    Former-commit-id: 50ada724b412bd0349fd7034245473de53117814

--- src/com/gwtplatform/mvp/client/PresenterWidgetImpl.java
+++ src/com/gwtplatform/mvp/client/PresenterWidgetImpl.java
@@ -93,7 +93,7 @@ public abstract class PresenterWidgetImpl<V extends View> extends PresenterWidge
    * Use {@link PresenterWidget#addToPopupSlot(PresenterWidget, boolean)} instead.
    */
   @Deprecated
-  public void addToPopupSlot(
+  public void addPopupContent(
       final PresenterWidget<? extends PopupView> content, boolean center) {
     addToPopupSlot(content, center);
   }
