9e2ef0d4ccdb23a68cf92630ea5cafb4251fac64
Brendan Doherty <brendanpdoherty@gmail.com>
2010-08-28 10:20:35 +1200

    Improve javadoc for GenDto
    --HG--
    branch : Issue 165
    
    
    Former-commit-id: 91a150d01348d31f2649da8a37d0e1ed935d6d00

--- src/com/gwtplatform/annotation/GenDto.java
+++ src/com/gwtplatform/annotation/GenDto.java
@@ -23,33 +23,58 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation to generate simple Data Transfer Object (DTO) classes.
+ * Annotation to generate simple Data Transfer Object (DTO) classes solely for
+ * transferring data between the client and the server.
  * <p/>
  * If you type:
  * 
  * <pre>
  * <code> 
  * {@literal}@GenDto
- * public class PersonName {
- *   String firstName;
- *   String lastName; 
+ * public class LineItem {
+ *   String Key&lt;Product&gt; productKey;
+ *   int quantity; 
  * }
  * </code>
  * </pre>
  * 
- * gwt-platform will generate a class call PersonNameDto.
+ * gwt-platform will generate a LineItemDto class.
  * <p/>
- * PersonNameDto will have fields, getters, and a constructor that 
- * takes firstName and lastName plus equals, hashCode, toString etc, 
+ * LineItemDto will have fields, getters, and a constructor that takes
+ * productKey and quantity plus equals, hashCode, toString etc,
+ * <p/>
+ * LineItemDto could be used when creating an invoice.
+ * 
+ * <pre>
+ * <code> 
+ * {@literal}@GenDispatch
+ * public class CreateInvoice {
+ *   {@literal}@In Key&lt;Customer&gt; customerKey
+ *   {@literal}@In LineItemDto[] lineItems
+ *   {@literal}@Out Invoice invoice;
+ * }
+ * </code>
+ * </pre>
+ * 
+ * The alternative to using Dto classes would be to construct persistable
+ * LineItem objects on the client and send them as part of the gwt-rpc call.
+ * Using Dto classes is a better choice because:
+ * <ul>
+ * <li>Lower bandwidth.</li>
+ * <li>The client cannot be trusted. The LineItem persistable object will
+ * probably have a price field. The server may need ignore the price as it may
+ * have been tampered with.</li>
+ * </ul>
+ * 
  * <p/>
  * Notes:
  * <p/>
- * There is no naming requirement for your class name. 
- * It will be appended with Dto
+ * There is no naming requirement for your class name. It will be appended with
+ * Dto
  * 
  * <p/>
  * 
- * @author Brendan Doherty 
+ * @author Brendan Doherty
  * @author Stephen Haberman (concept)
  */
 
