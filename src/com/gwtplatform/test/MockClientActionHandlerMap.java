31274b502935840db92b10fe85ff99a054c6418e
Brendan Doherty <brendanpdoherty@gmail.com>
2010-09-17 22:44:58 +1200

    Support unit testing client-side action handlers.

---
+++ src/com/gwtplatform/test/MockClientActionHandlerMap.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.test;
+
+import com.gwtplatform.dispatch.client.actionhandler.ClientActionHandler;
+
+/**
+ * This is the interface that define the map of {@link Action} to
+ * {@link ClientActionHandler} for unit testing.
+ * 
+ * @param <A> Type of the associated {@link Action}
+ * @param <R> Type of the associated {@link Result}
+ * 
+ * @author Brendan Doherty
+ */
+public interface MockClientActionHandlerMap {
+
+  /**
+   * @return the {@link Action} class associated
+   */
+  Class<?> getActionClass();
+
+  /**
+   * @return the {@link ClientActionHandler} class associated
+   */
+  ClientActionHandler<?, ?> getClientActionHandler();
+
+}
