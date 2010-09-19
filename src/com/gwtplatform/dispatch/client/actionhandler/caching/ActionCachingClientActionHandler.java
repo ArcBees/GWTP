3566aaad92bcfe7b3751fef43c4a58fe1830127a
Sunny Gupta <none@none>
2010-09-19 22:39:32 +0530

    Added skeleton code for Caching ClientActionHandler
    --HG--
    branch : Issue 208

---
+++ src/com/gwtplatform/dispatch/client/actionhandler/caching/ActionCachingClientActionHandler.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.dispatch.client.actionhandler.caching;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import com.gwtplatform.dispatch.client.ClientDispatchRequest;
+import com.gwtplatform.dispatch.client.actionhandler.UndoCommand;
+import com.gwtplatform.dispatch.shared.Action;
+import com.gwtplatform.dispatch.shared.Result;
+
+/**
+ * 
+ * @author Sunny Gupta
+ *
+ * @param <A>
+ * @param <R>
+ */
+public class ActionCachingClientActionHandler<A extends Action<R>, R extends Result> extends
+    AbstractCachingClientActionHandler<A, R> {
+
+  public ActionCachingClientActionHandler(Class<A> actionType, Cache cache) {
+    super(actionType, cache);
+  }
+
+  @Override
+  public void postfetch(A action, R result) {
+    // Cache
+    super.cache.put(action, result);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public R prefetch(A action) {
+    try {
+      // Check if Action available in Cache
+      Object value = super.cache.get(action);
+      if (value != null && value instanceof Result) {
+        return (R) value;
+      } else {
+        return null;
+      }
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @Override
+  public void undo(A action, R result, AsyncCallback<Void> callback,
+      ClientDispatchRequest request, UndoCommand<A, R> undoCommand) {
+    // TODO Need to think about it
+  }
+
+}
---
+++ src/com/gwtplatform/dispatch/client/actionhandler/caching/Cache.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.dispatch.client.actionhandler.caching;
+
+/**
+ * Interface for client side caching. This is kept different from generic Cache interfaces for simplicity
+ * 
+ * @author Sunny Gupta
+ */
+public interface Cache {
+  void put(Object key, Object value);
+  
+  Object get(Object key);
+}
