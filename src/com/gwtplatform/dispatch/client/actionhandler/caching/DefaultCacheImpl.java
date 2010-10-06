6c3f5b97e666c80ce815c944424e2a47a294174c
Sunny Gupta <none@none>
2010-10-06 09:05:53 +0530

    Updated Cache to provide flexibility of auto-expiration
    Renamed CachingClientActionHandler again
    
    --HG--
    branch : Issue 208
    rename : src/com/gwtplatform/dispatch/client/actionhandler/caching/CachingClientActionHandler.java => src/com/gwtplatform/dispatch/client/actionhandler/caching/ActionCachingHandler.java

--- src/com/gwtplatform/dispatch/client/actionhandler/caching/DefaultCacheImpl.java
+++ src/com/gwtplatform/dispatch/client/actionhandler/caching/DefaultCacheImpl.java
@@ -16,8 +16,6 @@
 
 package com.gwtplatform.dispatch.client.actionhandler.caching;
 
-import com.google.inject.Inject;
-
 import java.util.Date;
 import java.util.HashMap;
 
@@ -30,29 +28,57 @@ public class DefaultCacheImpl implements Cache {
 
   private class CacheValue {
 
-    private Object value;
-    private Date lastUpdateTime;
+    private final Object value;
+    private final long lastUpdateTime;
+    private final boolean autoExpire;
+    private final long expirationTimeInMs;
     
-    public CacheValue(Object value) {
+    public CacheValue(final Object value, final boolean autoExpire, long expirationTimeInMs) {
       this.value = value;
-      this.lastUpdateTime = new Date();
+      this.lastUpdateTime = new Date().getTime();
+      this.autoExpire = autoExpire;
+      this.expirationTimeInMs = expirationTimeInMs;
     }
 
     public Object getValue() {
       return this.value;
     }
 
-    public Date getLastUpateTime() {
+    public long getLastUpateTime() {
       return this.lastUpdateTime;
+    }
+
+    public boolean isAutoExpire() {
+      return autoExpire;
+    }
+
+    public long getExpirationTimeInMs() {
+      return expirationTimeInMs;
     }  
   }
   
   private HashMap<Object, CacheValue> map;
+  private long autoExpireTimeInMs;
   
-  @Inject
+  /**
+   * Initializes the cache with auto expiration OFF.
+   */
   public DefaultCacheImpl() {
     // Initialize map
-    map = new HashMap<Object, CacheValue>();
+    this.map = new HashMap<Object, CacheValue>();
+    // By default, autoExpireTime is 0 so that objects never expire
+    this.autoExpireTimeInMs = 0;
+  }
+  
+  /**
+   * Initialize the cache with auto expiration ON.
+   * 
+   * @param autoExpireTimeInMs Time in milliseconds after which entries in cache expire
+   */
+  public DefaultCacheImpl(long autoExpireTimeInMs) {
+    // Initialize map
+    this.map = new HashMap<Object, CacheValue>();
+    this.autoExpireTimeInMs = autoExpireTimeInMs;
   }
   
   @Override
@@ -64,45 +90,90 @@ public class DefaultCacheImpl implements Cache {
   public Object get(Object key) {
     // Check for null as Cache should not store null values / keys
     if (key == null) {
-      // TODO Find a way to throw NullArgumentException, maybe create a custom one for gwtp?
-      // throw new NullArgumentException("key");
-      throw new RuntimeException("key is null");
-   }
-    return map.get(key).getValue();
+      throw new NullPointerException("key is null");
+    }
+    CacheValue cacheValue = map.get(key);
+    // First check if entry can auto-expire
+    if (cacheValue.isAutoExpire()) {
+      // Now check if expirationTimeInMs is positive
+      long expireTime = 0;
+      if (cacheValue.getExpirationTimeInMs() > 0) {
+        expireTime = cacheValue.getExpirationTimeInMs();
+      } else {
+        // Apply default behaviour of cache: Check if autoexpired
+        if (cacheValue.isAutoExpire() && this.autoExpireTimeInMs > 0) {
+          expireTime = this.autoExpireTimeInMs;
+        }
+      }
+      // Check if expired
+      long now = new Date().getTime();
+      if (cacheValue.getLastUpateTime() + expireTime > now) {
+        // Expired, remove
+        remove(key);
+        return null;
+      }
+    }
+    
+    // Not expired, return the value
+    return cacheValue.getValue();
   }
 
   @Override
   public void put(Object key, Object value) {
+    // Put with autoExpire = true
+    put(key, value, true, 0);
+  }
+  
+  @Override
+  public void put(Object key, Object value, boolean autoExpire) {
+    // Put with autoExpire = true
+    put(key, value, autoExpire, 0);
+  }
+
+  @Override
+  public void put(Object key, Object value, long expirationTimeInMs) {
+    // Put with autoExpire = true
+    put(key, value, true, expirationTimeInMs);
+  }
+  
+  private void put(Object key, Object value, boolean autoExpire, long expirationTimeInMs) {
     // Check for null as Cache should not store null values / keys
     if (key == null) {
-      // TODO Find a way to throw NullArgumentException, maybe create a custom one for gwtp?
-      // throw new NullArgumentException("key");
-      throw new RuntimeException("key is null");
+      throw new NullPointerException("key is null");
     }
     if (value == null) {
-      // TODO Find a way to throw NullArgumentException, maybe create a custom one for gwtp?
-      // throw new NullArgumentException("key");
-      throw new RuntimeException("value is null");
+      throw new NullPointerException("value is null");
     }
     
     // Put in map
-    map.put(key, new CacheValue(value));
+    map.put(key, new CacheValue(value, autoExpire, expirationTimeInMs));
   }
-
+  
   @Override
   public void remove(Object key) {
     map.remove(key);
   }
 
   @Override
-  public Date getLastUpateTime(Object key) {
+  public long getLastUpateTime(Object key) {
     // Check for null as Cache should not store null values / keys
     if (key == null) {
-      // TODO Find a way to throw NullArgumentException, maybe create a custom one for gwtp?
-      // throw new NullArgumentException("key");
-      throw new RuntimeException("key is null");
+      throw new NullPointerException("key is null");
     }
     return map.get(key).getLastUpateTime();
+  }
+
+  @Override
+  public long getAutoExpireTimeInMs() {
+    return autoExpireTimeInMs;
+  }
+
+  @Override
+  public void setAutoExpireTimeInMs(long autoExpireTimeInMs) {
+    // Check for a positive value
+    if (autoExpireTimeInMs >= 0) {
+      this.autoExpireTimeInMs = autoExpireTimeInMs;
+    }
   }
 
 }
