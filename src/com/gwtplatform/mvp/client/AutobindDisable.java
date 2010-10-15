caa978cb0d739dd3efe89498456150b096d81d6c
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2010-10-14 21:38:34 -0700

    Corrected a bug that completely disabled automatic binding.

--- src/com/gwtplatform/mvp/client/AutobindDisable.java
+++ src/com/gwtplatform/mvp/client/AutobindDisable.java
@@ -16,17 +16,35 @@
 
 package com.gwtplatform.mvp.client;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
 /**
  * Bind this class to indicate that you wish to globally disable automatic binding in
  * classes derived from {@link HandlerContainerImpl}, such as {@link PresenterWidget} 
  * or {@link Presenter}. Simply bind it like this: 
  * <pre>
- * bind(AutobindDisable.class);
+ * bind(AutobindDisable.class).toInstance(new AutobindDisable(true));
  * </pre>
  * If you do not bind this class then autobinding is controlled on a case-by-case
  * basis using the {@link HandlerContainerImpl}'s constructors.
  * 
  * @author Philippe Beaudoin
  */
+@Singleton
 public class AutobindDisable {
+  private final boolean disable;
+
+  @Inject
+  AutobindDisable() {
+    disable = false;
+  }
+
+  public AutobindDisable(boolean disable) {
+    this.disable = disable;
+  }
+  
+  public boolean disable() {
+    return disable;
+  }
 }
