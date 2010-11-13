961a219e9d48114df136ea8b266a67eb923eb00c
Travis Camechis <camechis@gmail.com>
2010-11-13 11:29:17 -0500

    removed the support for adding a handler to make it compatiblie with GAE

--- src/com/gwtplatform/dispatch/server/spring/LoggerFactoryBean.java
+++ src/com/gwtplatform/dispatch/server/spring/LoggerFactoryBean.java
@@ -2,34 +2,28 @@ package com.gwtplatform.dispatch.server.spring;
 
 import org.springframework.beans.factory.FactoryBean;
 
-import java.util.List;
-import java.util.logging.Handler;
 import java.util.logging.Logger;
 
 public class LoggerFactoryBean implements FactoryBean {
 
-    private final Logger logger;
+  private final Logger logger;
 
-    public LoggerFactoryBean(java.util.logging.Logger logger,
-            List<Handler> handlers) {
-        this.logger = logger;
-        for (Handler handler : handlers) {
-            logger.addHandler(handler);
-        }
-    }
+  public LoggerFactoryBean(java.util.logging.Logger logger) {
+    this.logger = logger;
+  }
 
-    @Override
-    public Object getObject() throws Exception {
-        return logger;
-    }
+  @Override
+  public Object getObject() throws Exception {
+    return logger;
+  }
 
-    @Override
-    public Class getObjectType() {
-        return Logger.class;
-    }
+  @Override
+  public Class getObjectType() {
+    return Logger.class;
+  }
 
-    @Override
-    public boolean isSingleton() {
-        return true;
-    }
+  @Override
+  public boolean isSingleton() {
+    return true;
+  }
 }
