3a1098cda05d547f242441f73c66a80338fce09f
Christian Goudreau <none@none>
2010-11-13 13:18:24 -0500

    Update issue 263 Added MockingBinder inside Gwt-Platform project. In addition to MockingBinder, a MockFactory interface has been introduced to let you use any Mock framework you want.
    Made some unit test to see if field where mocked as expected.
    
    
    Former-commit-id: 8699bfeae508d03c4bd420f0c1537f92c05ea42e

---
+++ test/com/gwtplatform/tester/MockitoMockFactory.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.tester;
+
+import org.mockito.Mockito;
+
+/**
+ * Mock factory that use Mockito.
+ * 
+* @author Christian Goudreau
+*/
+public class MockitoMockFactory implements MockFactory {
+  @Override
+  public <T> T mock(Class<T> classToMock) {
+    return Mockito.mock(classToMock);
+  }
+}
