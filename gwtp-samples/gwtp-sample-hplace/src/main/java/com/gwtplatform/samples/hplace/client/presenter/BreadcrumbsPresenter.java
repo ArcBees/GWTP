6a2255c06874a16cecec090d2d7a0a94b8301997
Yannis Gonianakis <jgonian@gmail.com>
2010-11-23 23:56:59 +0200

    Ported hplace sample to maven.
    Configured annotation processing to take place, in order for required Action/Result sources to be generated.
    Sources from annotation processing will be generated into target/generated-sources/apt.

---
+++ gwtp-samples/gwtp-sample-hplace/src/main/java/com/gwtplatform/samples/hplace/client/presenter/BreadcrumbsPresenter.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.hplace.client.presenter;
+
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.inject.Inject;
+
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.ContentSlot;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
+import com.gwtplatform.mvp.client.proxy.Proxy;
+import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
+import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
+import com.gwtplatform.mvp.client.proxy.SetPlaceTitleHandler;
+
+/**
+ * This is the top-level presenter of the hierarchy. Other presenters reveal
+ * themselves within this presenter. This presenter display a breadcrumbs with
+ * the titles of the previously visited pages.
+ * 
+ * @author Christian Goudreau
+ * @author Philippe Beaudoin
+ */
+public class BreadcrumbsPresenter extends
+    Presenter<BreadcrumbsPresenter.MyView, BreadcrumbsPresenter.MyProxy> {
+  /**
+   * {@link BreadcrumbsPresenter}'s proxy.
+   */
+  @ProxyStandard
+  public interface MyProxy extends Proxy<BreadcrumbsPresenter> {
+  }
+
+  /**
+   * {@link BreadcrumbsPresenter}'s view.
+   */
+  public interface MyView extends View {
+    void clearBreadcrumbs(int breadcrumbSize);
+    void setBreadcrumbs(int index, String title);
+  }
+
+  /**
+   * Use this in leaf presenters, inside their {@link #revealInParent} method.
+   */
+  @ContentSlot
+  public static final Type<RevealContentHandler<?>> TYPE_SetMainContent = new Type<RevealContentHandler<?>>();
+
+  private final PlaceManager placeManager;
+
+  @Inject
+  public BreadcrumbsPresenter(final EventBus eventBus, final MyView view,
+      final MyProxy proxy, final PlaceManager placeManager) {
+    super(eventBus, view, proxy);
+    this.placeManager = placeManager;
+  }
+
+  @Override
+  protected void onReset() {
+    super.onReset();
+    int size = placeManager.getHierarchyDepth();
+    getView().clearBreadcrumbs(size);
+    for (int i = 0; i < size; ++i) {
+      final int index = i;
+      placeManager.getTitle(i, new SetPlaceTitleHandler() {
+        @Override
+        public void onSetPlaceTitle(String title) {
+          getView().setBreadcrumbs(index, title);
+        }
+      });
+    }
+  }
+
+  @Override
+  protected void revealInParent() {
+    RevealRootContentEvent.fire(this, this);
+  }
+}
---
+++ gwtp-samples/gwtp-sample-hplace/src/main/java/com/gwtplatform/samples/hplace/client/view/HomeView.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.hplace.client.view;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import com.gwtplatform.samples.hplace.client.presenter.HomePresenter.MyView;
+
+/**
+ * @author Philippe Beaudoin
+ */
+public class HomeView extends ViewWithUiHandlers<HomeUiHandlers> 
+  implements MyView {  
+
+  interface HomeViewUiBinder extends UiBinder<Widget, HomeView> {
+  }
+
+  private static HomeViewUiBinder uiBinder = GWT.create(HomeViewUiBinder.class);
+  
+  @UiField
+  Button all;
+
+  @UiField
+  Button favorites;
+
+  @UiField
+  Button specials;
+
+  private final Widget widget;
+
+  @Inject
+  public HomeView() {
+    widget = uiBinder.createAndBindUi(this);
+  }
+
+  @Override
+  public Widget asWidget() {
+    return widget;
+  }
+  
+  @UiHandler("all")
+  void onAllClicked(ClickEvent event) {
+    getUiHandlers().revealAllProductsList();
+  }
+  
+  @UiHandler("favorites")
+  void onFavoritesClicked(ClickEvent event) {
+    getUiHandlers().revealFavoriteProductsList();
+  }
+
+  @UiHandler("specials")
+  void onSpecialsClicked(ClickEvent event) {
+    getUiHandlers().revealSpecialsList();
+  }
+  
+}
