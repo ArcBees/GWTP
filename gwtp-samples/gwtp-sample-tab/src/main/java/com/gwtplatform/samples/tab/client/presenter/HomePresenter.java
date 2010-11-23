8c1fc662fd67c1427ba3e1ceb8c5f61a792a2628
Yannis Gonianakis <jgonian@gmail.com>
2010-11-24 01:09:42 +0200

    Ported tab sample to maven.
    Former-commit-id: a8d0cb89306fc9b4e82a39a28ca780a1c58c461b

---
+++ gwtp-samples/gwtp-sample-tab/src/main/java/com/gwtplatform/samples/tab/client/presenter/HomePresenter.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.tab.client.presenter;
+
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.inject.Inject;
+
+import com.gwtplatform.mvp.client.RequestTabsHandler;
+import com.gwtplatform.mvp.client.TabContainerPresenter;
+import com.gwtplatform.mvp.client.TabView;
+import com.gwtplatform.mvp.client.annotations.ContentSlot;
+import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
+import com.gwtplatform.mvp.client.annotations.RequestTabs;
+import com.gwtplatform.mvp.client.annotations.TabInfo;
+import com.gwtplatform.mvp.client.proxy.RevealContentEvent;
+import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
+import com.gwtplatform.mvp.client.proxy.TabContentProxy;
+import com.gwtplatform.samples.tab.client.NameTokens;
+
+/**
+ * @author Christian Goudreau
+ */
+public class HomePresenter extends
+    TabContainerPresenter<HomePresenter.MyView, HomePresenter.MyProxy> {
+  /**
+   * {@link HomePresenter}'s proxy.
+   */
+  @ProxyCodeSplit
+  @TabInfo(container = MainPagePresenter.class, priority = 0, // The first tab
+                                                              // in the home tab
+  label = "Home", nameToken = NameTokens.homePage)
+  public interface MyProxy extends TabContentProxy<HomePresenter> {
+  }
+
+  /**
+   * {@link HomePresenter}'s view.
+   */
+  public interface MyView extends TabView {
+  }
+
+  /**
+   * This will be the event sent to our "unknown" child presenters, in order for
+   * them to register their tabs.
+   */
+  @RequestTabs
+  public static final Type<RequestTabsHandler> TYPE_RequestTabs = new Type<RequestTabsHandler>();
+
+  /**
+   * Use this in leaf presenters, inside their {@link #revealInParent} method.
+   */
+  @ContentSlot
+  public static final Type<RevealContentHandler<?>> TYPE_SetTabContent = new Type<RevealContentHandler<?>>();
+
+  @Inject
+  public HomePresenter(final EventBus eventBus, final MyView view,
+      final MyProxy proxy) {
+    super(eventBus, view, proxy, TYPE_SetTabContent, TYPE_RequestTabs);
+  }
+
+  @Override
+  protected void revealInParent() {
+    RevealContentEvent.fire(this, MainPagePresenter.TYPE_SetTabContent,
+        this);
+  }
+}
---
+++ gwtp-samples/gwtp-sample-tab/src/main/java/com/gwtplatform/samples/tab/client/ui/BaseTab.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.tab.client.ui;
+
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.Widget;
+
+import com.gwtplatform.mvp.client.Tab;
+
+/**
+ * @author Christian Goudreau
+ */
+public abstract class BaseTab extends Composite implements Tab {
+  /**
+   * {@link BaseTab}'s CssResource.
+   */
+  protected interface Style extends CssResource {
+    String active();
+
+    String inactive();
+  }
+
+  @UiField
+  Hyperlink hyperlink;
+
+  @UiField
+  Style style;
+
+  private final float priority;
+
+  public BaseTab(float priority) {
+    super();
+    this.priority = priority;
+  }
+
+  @Override
+  public void activate() {
+    removeStyleName(style.inactive());
+    addStyleName(style.active());
+  }
+
+  @Override
+  public Widget asWidget() {
+    return this;
+  }
+
+  @Override
+  public void deactivate() {
+    removeStyleName(style.active());
+    addStyleName(style.inactive());
+  }
+
+  @Override
+  public float getPriority() {
+    return priority;
+  }
+
+  @Override
+  public String getText() {
+    return hyperlink.getText();
+  }
+
+  @Override
+  public void setTargetHistoryToken(String historyToken) {
+    hyperlink.setTargetHistoryToken(historyToken);
+  }
+
+  @Override
+  public void setText(String text) {
+    hyperlink.setText(text);
+  }
+
+}
