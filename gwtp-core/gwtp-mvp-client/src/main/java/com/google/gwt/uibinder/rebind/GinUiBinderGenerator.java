35b114889f835569f4ad7b1bf9a19d02c5544dcb
Yannis Gonianakis <jgonian@gmail.com>
2010-12-12 19:51:22 +0200

    Added checkstyle suppressions for files under com/google and reverted some mistakenly committed changes from revision 883f0eaf8b.
    Former-commit-id: 5a7bcc5927d1dae7ee82a6e7defe49b946b9b30c

--- gwtp-core/gwtp-mvp-client/src/main/java/com/google/gwt/uibinder/rebind/GinUiBinderGenerator.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/google/gwt/uibinder/rebind/GinUiBinderGenerator.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2010 ArcBees Inc.
+/*
+ * Copyright 2008 Google Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -191,6 +191,7 @@ public class GinUiBinderGenerator extends Generator {
     try {
       String ginjectorClassName = propertyOracle.getConfigurationProperty(
           "gin.ginjector").getValues().get(0);
+      
 
       JClassType ginjectorClass = oracle.findType(ginjectorClassName);
       if (ginjectorClass == null
