8a5786c8bb2da049859791b6a2161401498b154a
goudreau.christian@gmail.com <goudreau.christian@gmail.com>
2011-05-01 16:46:58 -0400

    Added Google Analytics support.

---
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/googleanalytics/GoogleAnalyticsImpl.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.googleanalytics;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.ScriptElement;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Default {@link GoogleAnalytics} implementation that uses JSNI to
+ * expose Google Analytics javascript methods.
+ *
+ * @author Christian Goudreau
+ */
+public class GoogleAnalyticsImpl implements GoogleAnalytics {
+  @Override
+  public native void trackEvent(String category, String action) /*-{
+    $wnd._gaq.push([ '_trackEvent', category, action ]);
+  }-*/;
+
+  @Override
+  public native void trackEvent(String category, String action,
+      String optLabel, int optValue) /*-{
+    $wnd._gaq.push([ '_trackEvent', category, action, optLabel, optValue ]);
+  }-*/;
+
+  public native void trackPageview(String pageName) /*-{
+    if (!pageName.match("^/") == "/") {
+      pageName = "/" + pageName;
+    }
+
+    $wnd._gaq.push([ '_trackPageview', pageName ]);
+  }-*/;
+
+  @Override
+  public native void trackPageview() /*-{
+    $wnd._gaq.push([ '_trackPageview' ]);
+  }-*/;
+
+  @Override
+  public void init(String userAccount) {
+    ScriptElement config = Document.get().createScriptElement(
+        "var _gaq = _gaq || [];_gaq.push(['_setAccount', '" + userAccount
+            + "']);_gaq.push(['_trackPageview']);");
+    Element firstscript = Document.get().getElementsByTagName("script").getItem(
+        0);
+    firstscript.getParentNode().insertBefore(config, firstscript);
+
+    ScriptElement script = Document.get().createScriptElement();
+    script.setSrc(("https:".equals(Window.Location.getProtocol())
+        ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js");
+    script.setType("text/javascript");
+    script.setAttribute("async", "true");
+    firstscript.getParentNode().insertBefore(script, firstscript);
+  }
+
+  @Override
+  public native void addAccount(String trackerName, String userAccount) /*-{
+    $wnd._gaq.push([ '" + trackerName + "._setAccount', '" + userAccount + "' ]);
+  }-*/;
+
+  @Override
+  public native void trackPageview(String trackerName, String pageName) /*-{
+     if (!pageName.match("^/") == "/") {
+      pageName = "/" + pageName;
+    }
+
+    $wnd._gaq.push([ '" + trackerName + "._trackPageview', pageName ]);
+  }-*/;
+
+  @Override
+  public native void trackEvent(String trackerName, String category, String action) /*-{
+    $wnd._gaq.push([ '" + trackerName + "._trackEvent', category, action ]);
+  }-*/;
+
+  @Override
+  public native void trackEvent(String trackerName, String category, String action,
+      String optLabel, int optValue) /*-{
+    $wnd._gaq.push([ '" + trackerName + "._trackEvent', category, action, optLabel, optValue ]);
+  }-*/;
+}
---
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/googleanalytics/GoogleAnalyticsNavigationTracker.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.googleanalytics;
+
+import javax.inject.Inject;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.EventBus;
+import com.gwtplatform.mvp.client.annotations.GaAccount;
+import com.gwtplatform.mvp.client.proxy.NavigationEvent;
+import com.gwtplatform.mvp.client.proxy.NavigationHandler;
+
+/**
+ * This class let's you register every navigation event to a Google Analytics
+ * account. To use it, you must bind GoogleAnalytics as eager singleton in your
+ * gin module and also bind the annotation {@link GaAccount} to your Google
+ * Analytics account number:
+ * <p />
+ * <code>bind(GoogleAnalyticsImpl.class).to(GoogleAnalytics.class).asEagerSingleton();
+ * bindConstant().annotatedWith(GaAccount.class).to("UA-12345678-1");</code>
+ * <p />
+ * If you want to log custom events, see {@link GoogleAnalytics}.
+ *
+ * @author Christian Goudreau
+ */
+public class GoogleAnalyticsNavigationTracker implements NavigationHandler {
+  private final GoogleAnalytics analytics;
+
+  @Inject
+  public GoogleAnalyticsNavigationTracker(@GaAccount final String gaAccount,
+      final EventBus eventBus, final GoogleAnalytics analytics) {
+    this.analytics = analytics;
+
+    if (GWT.isScript()) {
+      analytics.init(gaAccount);
+
+      eventBus.addHandler(NavigationEvent.getType(), this);
+    }
+  }
+
+  @Override
+  public void onNavigation(NavigationEvent navigationEvent) {
+    analytics.trackPageview(navigationEvent.getRequest().getNameToken());
+  }
+}
