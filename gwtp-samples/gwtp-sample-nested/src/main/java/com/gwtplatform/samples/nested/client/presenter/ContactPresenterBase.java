5f91dea27cac86cca4462f1ba539a52529b7b50c
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2011-05-30 17:07:06 -0400

    Fixed. Issue 180: @ProxyEvent declared in parent class are ignored by child class http://code.google.com/p/jukito/issues/detail?id=180
    Former-commit-id: e1cc85f40a7899a49b215a2a77b55d606472d7b5

---
+++ gwtp-samples/gwtp-sample-nested/src/main/java/com/gwtplatform/samples/nested/client/presenter/ContactPresenterBase.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.nested.client.presenter;
+
+import com.google.gwt.event.shared.EventBus;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.ProxyEvent;
+import com.gwtplatform.mvp.client.proxy.NavigationEvent;
+import com.gwtplatform.mvp.client.proxy.Proxy;
+
+/**
+ * The base class of {@link ContactPresenter}. The goal of this class is just to show that
+ * {@code @ProxyEvent} can be used in base classes. See Issue 180.
+ *
+ * @author Philippe Beaudoin
+ *
+ * @param <Proxy_> The {@link Proxy} type.
+ */
+public abstract class ContactPresenterBase<Proxy_ extends Proxy<?>>
+    extends Presenter<ContactPresenterBase.MyView, Proxy_> {
+
+  String navigationHistory = "";
+
+  /**
+   * {@link ContactPresenter}'s view.
+   */
+  public interface MyView extends View {
+    void setNavigationHistory(String navigationHistory);
+  }
+
+  public ContactPresenterBase(EventBus eventBus, MyView view, Proxy_ proxy) {
+    super(eventBus, view, proxy);
+  }
+
+  /**
+   * We keep track of the previously visited pages.
+   *
+   * @param event The
+   *          {@link com.gwtplatform.mvp.client.proxy.LockInteractionEvent}.
+   */
+  @ProxyEvent
+  public void onNavigation(NavigationEvent event) {
+    if (navigationHistory.length() > 0) {
+      navigationHistory += ", ";
+    }
+    navigationHistory += event.getRequest().getNameToken();
+    getView().setNavigationHistory(navigationHistory);
+  }
+}
