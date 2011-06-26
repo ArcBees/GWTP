1c35dc015d0d8a5979ce768bfe9db0a30b836568
Florian Sauter <floonweb@googlemail.com>
2011-06-26 18:36:59 +0200

    Included debug messages (Issue 318).

--- gwtp-core/gwtp-processors/src/main/java/com/gwtplatform/dispatch/annotation/processor/GenProcessor.java
+++ gwtp-core/gwtp-processors/src/main/java/com/gwtplatform/dispatch/annotation/processor/GenProcessor.java
@@ -16,6 +16,7 @@
 
 package com.gwtplatform.dispatch.annotation.processor;
 
+import java.lang.annotation.Annotation;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -23,6 +24,7 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic.Kind;
 
 /**
  * <p>
@@ -57,19 +59,45 @@ public abstract class GenProcessor extends AbstractProcessor {
   }
   
   @Override
+  @SuppressWarnings("unchecked")
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
     if (!roundEnvironment.processingOver()) {
-      for (TypeElement currentAnnotation : annotations) {
-        for (String supportedAnnotationName : getSupportedAnnotationTypes()) {
-          if (currentAnnotation.getQualifiedName().contentEquals(supportedAnnotationName)) {
-            for (Element annotatedElement : roundEnvironment.getElementsAnnotatedWith(currentAnnotation)) {
-              this.process(annotatedElement);
+      onProcessingStarted();
+      for (String supportedAnnotationName : getSupportedAnnotationTypes()) {
+        printMessage("Searching for " + supportedAnnotationName + " annotations.");
+        try {
+            Class<?> supportedAnnotationClass = Class.forName(supportedAnnotationName);
+            if (supportedAnnotationClass.isAnnotation()) {
+              for (Element annotatedElement : roundEnvironment.getElementsAnnotatedWith((Class<? extends Annotation>) supportedAnnotationClass)) {
+                printMessage("Found " + annotatedElement.toString() + ".");
+                this.process(annotatedElement);
+              }
             }
-          }
+        } catch (ClassNotFoundException e) {
+          printError("Annotation not found: " + supportedAnnotationName);
         }
       }
+      onProcessingCompleted();
     }
     return true;
+  }
+  
+  /**
+   * Prints a message.
+   * 
+   * @param message the message
+   */
+  public void printMessage(String message) {
+    getEnvironment().getMessager().printMessage(Kind.NOTE,  message);
+  }
+  
+  /**
+   * Prints an error.
+   * 
+   * @param message the error message
+   */
+  public void printError(String message) {
+    getEnvironment().getMessager().printMessage(Kind.ERROR,  message);
   }
   
   /**
@@ -79,4 +107,18 @@ public abstract class GenProcessor extends AbstractProcessor {
    * @param annotatedElement the annotated element.
    */
   public abstract void process(Element annotatedElement);
+  
+  /**
+   * Utility method called after processing has started.
+   */
+  public void onProcessingStarted() {
+    printMessage(getClass().getName() + " started.");
+  }
+  
+  /**
+   * Utility method called after the processing is finished.
+   */
+ public void onProcessingCompleted() {
+   printMessage(getClass().getName() + " finished.");
+  }
 }
