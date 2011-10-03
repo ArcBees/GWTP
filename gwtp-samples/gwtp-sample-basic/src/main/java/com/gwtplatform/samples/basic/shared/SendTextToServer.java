258afdf67aabc51cf0fce11841687758e487fb72
Philippe Beaudoin <none@none>
2011-10-03 01:41:57 -0400

    Complete. Issue 363: Make GWTP development work seamlessly in Eclipse 3.7 Indigo http://code.google.com/p/gwt-platform/issues/detail?id=363

--- gwtp-samples/gwtp-sample-basic/src/main/java/com/gwtplatform/samples/basic/shared/SendTextToServer.java
+++ gwtp-samples/gwtp-sample-basic/src/main/java/com/gwtplatform/samples/basic/shared/SendTextToServer.java
@@ -26,8 +26,6 @@ import com.gwtplatform.dispatch.shared.UnsecuredActionImpl;
 public class SendTextToServer extends
     UnsecuredActionImpl<SendTextToServerResult> {
 
-  private static final long serialVersionUID = 4621412923270714515L;
-
   private String textToServer;
 
   public SendTextToServer(final String textToServer) {
--- gwtp-samples/gwtp-sample-basic/src/main/java/com/gwtplatform/samples/basic/shared/SendTextToServerResult.java
+++ gwtp-samples/gwtp-sample-basic/src/main/java/com/gwtplatform/samples/basic/shared/SendTextToServerResult.java
@@ -23,8 +23,6 @@ import com.gwtplatform.dispatch.shared.Result;
  */
 public class SendTextToServerResult implements Result {
 
-  private static final long serialVersionUID = 4621412923270714515L;
-
   private String response;
 
   public SendTextToServerResult(final String response) {
