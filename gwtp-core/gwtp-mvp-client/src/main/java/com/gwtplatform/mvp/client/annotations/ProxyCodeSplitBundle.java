97f99a57f9f202873a09dbb6d3e06b175750fc91
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2011-10-11 00:05:40 -0400

    Complete. Issue 203 : Add javadoc for annotations http://code.google.com/p/gwt-platform/issues/detail?id=203 ===================================================================
    Former-commit-id: 2a091b091555e7b8d5d9aa80a210774f95d52408

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/annotations/ProxyCodeSplitBundle.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/annotations/ProxyCodeSplitBundle.java
@@ -16,12 +16,37 @@
 
 package com.gwtplatform.mvp.client.annotations;
 
-import com.gwtplatform.common.client.ProviderBundle;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+import com.gwtplatform.common.client.ProviderBundle;
+
 /**
+ * Use this annotation if you want to have certain
+ * {@link com.gwtplatform.mvp.client.proxy.Proxy}s and their associated
+ * {@link com.gwtplatform.mvp.client.Presenter}s to sit behind one split point
+ * and to be compiled into one javascript file seperately from others.
+ * <p/>
+ * Use this annotation if you already have too much code splitting using
+ * {@link ProxyCodeSplit} and it is more efficient to group
+ * {@link com.gwtplatform.mvp.client.Presenter}s because they share a bulk of
+ * their code. You will also have to set up your own implementation of a
+ * {@link ProviderBundle}.
+ * <p/>
+ * Here is an example use of {@link ProxyCodeSplitBundle}:
+ * 
+ * <pre>
+ * &#064;ProxyCodeSplitBundle(bundleClass = MyPresenterBundle.class, id = MyPresenterBundle.ID_Object1)
+ * public interface MyProxy extends ProxyPlace&lt;Object1&gt; {
+ * }
+ * </pre>
+ * 
+ * 
+ * @see ProviderBundle
+ * @see <a
+ *      href="http://code.google.com/intl/de-DE/webtoolkit/doc/latest/DevGuideCodeSplitting.html">Code
+ *      Splitting</a>
+ * 
  * @author Philippe Beaudoin
  */
 @Target(ElementType.TYPE)
