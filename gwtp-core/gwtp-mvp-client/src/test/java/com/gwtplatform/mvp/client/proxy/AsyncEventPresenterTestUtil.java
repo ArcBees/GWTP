0efc8c4f950d2fae929f40e1bc04e892975d1fcb
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2011-11-03 16:39:41 -0400

    Integration test for AsyncCall methods ===================================================================
    Former-commit-id: 328e16a3fa530047ca3b1a3f79521cdf8c0c1207

---
+++ gwtp-core/gwtp-mvp-client/src/test/java/com/gwtplatform/mvp/client/proxy/AsyncEventPresenterTestUtil.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.proxy;
+
+import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.dispatch.shared.ActionImpl;
+import com.gwtplatform.dispatch.shared.DispatchAsync;
+import com.gwtplatform.dispatch.shared.Result;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.mvp.MainPresenterTestUtil;
+
+/**
+ * This is the test presenter which implements {@link AsyncCallFailHandler},
+ * {@link AsyncCallStartHandler} and {@link AsyncCallSucceedHandler} to capture
+ * all events we want to test.
+ * 
+ * @author bjoern.moritz
+ */
+public class AsyncEventPresenterTestUtil extends
+    Presenter<AsyncEventPresenterTestUtil.MyView, AsyncEventPresenterTestUtil.MyProxy> implements AsyncCallFailHandler,
+    AsyncCallStartHandler, AsyncCallSucceedHandler {
+
+  private DispatchAsync dispatcher;
+
+  /**
+   * Presenter's view.
+   */
+  public interface MyView extends View {
+    void setMessage(String string);
+  }
+
+  /**
+   * Presenter's proxy.
+   */
+  @ProxyStandard
+  public interface MyProxy extends Proxy<MainPresenterTestUtil> {
+  }
+
+  public AsyncEventPresenterTestUtil(EventBus eventBus, MyView view, MyProxy proxy, DispatchAsync dispatcher) {
+    super(eventBus, view, proxy);
+    this.dispatcher = dispatcher;
+  }
+
+  @Override
+  protected void onBind() {
+    super.onBind();
+    
+    registerHandler(addHandler(AsyncCallStartEvent.getType(), this));
+    registerHandler(addHandler(AsyncCallSucceedEvent.getType(), this));
+    registerHandler(addHandler(AsyncCallFailEvent.getType(), this));
+  }
+
+  void executeAsync() {
+    NotifyingAsyncCallback<MyResult> callback = new NotifyingAsyncCallback<MyResult>(getEventBus()) {
+      @Override
+      protected void success(MyResult result) {
+        // Nothing needed here, we only want to test successful event handling
+      }
+    };
+
+    callback.prepare();
+    // In contrast to JavaDoc for NotifyingAsyncCallback we need to call
+    // checkLoading before executing the server call; otherwise the server call
+    // will return before checkLoading could be executed.
+    callback.checkLoading();
+    dispatcher.execute(new MyAction(), callback);
+  }
+
+  @Override
+  public void onAsyncCallSucceed(AsyncCallSucceedEvent asyncCallSucceedEvent) {
+    getView().setMessage(null);
+  }
+
+  @Override
+  public void onAsyncCallStart(AsyncCallStartEvent asyncCallStartEvent) {
+    getView().setMessage("Loading...");
+  }
+
+  @Override
+  public void onAsyncCallFail(AsyncCallFailEvent asyncCallFailEvent) {
+    getView().setMessage("Oops, something went wrong...");
+  }
+
+  @Override
+  protected void revealInParent() {
+    RevealRootContentEvent.fire(this, this);
+  }
+
+  static class MyAction extends ActionImpl<MyResult> {
+    public MyAction() {
+    }
+  }
+
+  static class MyResult implements Result {
+    private MyResult() {
+    }
+  }
+}
