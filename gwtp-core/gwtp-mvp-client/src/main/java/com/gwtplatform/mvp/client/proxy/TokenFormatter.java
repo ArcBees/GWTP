1253fd0a05f55ca2c52639da6aa5b4c5daef9538
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2012-01-31 20:32:00 -0500

    Changed all line delimiters to standard UNIX ones.

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/proxy/TokenFormatter.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/proxy/TokenFormatter.java
@@ -19,28 +19,28 @@ package com.gwtplatform.mvp.client.proxy;
 import java.util.List;
 
 /**
- * Formats tokens from {@code String} values to {@link PlaceRequest} and
- * {@link PlaceRequest} hierarchies and vice-versa. An implementation is
- * provided in {@link ParameterTokenFormatter}, see the documentation of this
- * class for details.
+ * Formats tokens from {@code String} values to {@link PlaceRequest} and {@link PlaceRequest}
+ * hierarchies and vice-versa. An implementation is provided in {@link ParameterTokenFormatter}, see
+ * the documentation of this class for details.
  */
 public interface TokenFormatter {
 
   /**
-   * Converts a place request hierarchy (a list of {@link PlaceRequest}) into a
-   * history token.
+   * Converts a place request hierarchy (a list of {@link PlaceRequest}) into a history token. The
+   * history token will not be fully query encoded, it may still contain some unescaped separators.
+   * However, these separators can be encoded and it will still work, making the scheme robust to
+   * systems that force encoding these characters like some email apps.
    *
    * @param placeRequestHierarchy The place request hierarchy.
-   * @return The history token.
+   * @return The history token, URL encoded.
    * @throws TokenFormatException if there is an error converting.
    */
   String toHistoryToken(List<PlaceRequest> placeRequestHierarchy)
       throws TokenFormatException;
 
   /**
-   * Converts a place token into a {@link PlaceRequest}. A place token is
-   * different than a history token, since the history token can contain a
-   * hierarchy of different place requests.
+   * Converts a place token into a {@link PlaceRequest}. A place token is different than a history
+   * token, since the history token can contain a hierarchy of different place requests.
    *
    * @param placeToken The place token.
    * @return The place request.
@@ -49,8 +49,7 @@ public interface TokenFormatter {
   PlaceRequest toPlaceRequest(String placeToken) throws TokenFormatException;
 
   /**
-   * Converts a history token into a place request hierarchy (a list of
-   * {@link PlaceRequest}).
+   * Converts a history token into a place request hierarchy (a list of {@link PlaceRequest}).
    *
    * @param historyToken The history token.
    * @return The place request hierarchy.
@@ -60,12 +59,14 @@ public interface TokenFormatter {
       throws TokenFormatException;
 
   /**
-   * Converts a {@link PlaceRequest} into a place token. A place token is
-   * different than a history token, since the history token can contain a
-   * hierarchy of different place requests.
+   * Converts a {@link PlaceRequest} into a place token. A place token is different than a history
+   * token, since the history token can contain a hierarchy of different place requests. The history
+   * token will not be fully query encoded, it may still contain some unescaped separators.
+   * However, these separators can be encoded and it will still work, making the scheme robust to
+   * systems that force encoding these characters like some email apps.
    *
    * @param placeRequest The place request.
-   * @return The place token.
+   * @return The place token, URL encoded.
    * @throws TokenFormatException if there is an error converting.
    */
   String toPlaceToken(PlaceRequest placeRequest) throws TokenFormatException;
