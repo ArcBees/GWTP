2eb3a592a0088f84bb9cde581c8c8e2d1a6f4cfb
Brandon Donnelson <branflake2267@gmail.com>
2012-11-26 22:57:03 -0800

    Working on distribution of zip files/downloads, distribution folder.

--- release-zip.sh
+++ release-zip.sh
@@ -1,22 +1,47 @@
 #/bin/sh
 
-rm -f ../gwtp-samples-$GWTPVER.zip ../gwtp-separate-$GWTPVER.zip ../gwtp-all-$GWTPVER.zip
-rm -rf ../gwtp-samples/ ../gwtp-separate/ ../gwtp-all/
-hg update gwtp-$GWTPVER
-mvn clean
-find . -name ".?*" -a -not -name ".hg*" -exec rm -rf {} \;
-zip -r ../gwtp-samples-$GWTPVER.zip gwtp-samples
-mvn install
-cd ..
-REPO=~/.m2/repository/com/gwtplatform
-mkdir gwtp-all
-cp  $REPO/gwtp-all/$GWTPVER/*.jar gwtp-all
-cd gwtp-all
-zip ../gwtp-all-$GWTPVER.zip *.jar
-cd ..
-mkdir gwtp-separate
-for d in `ls -d $REPO/gwtp-clients* $REPO/gwtp-crawler* $REPO/gwtp-dispatch* $REPO/gwtp-mvp* $REPO/gwtp-processors* $REPO/gwtp-tester*`; do cp $d/$GWTPVER/*.jar gwtp-separate/; done
-cd gwtp-separate
-zip ../gwtp-separate-$GWTPVER.zip *.jar
-cd ..
-rm -rf gwtp-samples/ gwtp-separate/ gwtp-all/
+# Editable
+GWTPVER=0.8
+
+# Don't Edit Below *********
+CURRENTDIR=`pwd`
+DISTRIBUTION=$CURRENTDIR/distribution
+RELEASE=$DISTRIBUTION/release/com/gwtplatform
+ZIPDIR=$DISTRIBUTION/downloads
+
+echo "Started"
+
+# Clean House
+# Don't delete distribution if deploying to a repository
+rm -rf $DISTRIBUTION
+mkdir $DISTRIBUTION
+mkdir $ZIPDIR
+
+# Zip Samples
+zip -x ".*" -r $ZIPDIR/gwtp-samples-$GWTPVER.zip gwtp-samples
+
+# Maven Building
+
+# Build local snapshots maven repository
+#mvn -DaltDeploymentRepository=snapshot-repo::default::file:$DISTRIBUTION/snapshots clean deploy
+
+# Build local release
+mvn -Prelease -DaltDeploymentRepository=release::default::file://$DISTRIBUTION/release clean deploy
+
+# Copy and zip jars
+mkdir $ZIPDIR/gwtp-all
+cp $RELEASE/gwtp-all/*.jar $ZIPDIR/gwtp-all
+
+cd $ZIPDIR/gwtp-all
+zip -x ".*" $ZIPDIR/gwtp-all/gwtp-all-$GWTPVER.zip *.jar
+
+mkdir $ZIPDIR/gwtp-separate
+for d in `ls -d $RELEASE/gwtp-clients* $RELEASE/gwtp-crawler* $RELEASE/gwtp-dispatch* $RELEASE/gwtp-mvp* $RELEASE/gwtp-processors* $RELEASE/gwtp-tester*`; do cp $d/$GWTPVER/*.jar $ZIPDIR/gwtp-separate/; done
+
+cd $ZIPDIR/gwtp-separate
+zip -x ".*" $ZIPDIR/gwtp-separate/gwtp-separate-$GWTPVER.zip *.jar
+
+# Remove tmp directories
+rm -rf $ZIPDIR/gwtp-samples/ $ZIPDIR/gwtp-separate/ $ZIPDIR/gwtp-all/
+
+echo "Finished"
