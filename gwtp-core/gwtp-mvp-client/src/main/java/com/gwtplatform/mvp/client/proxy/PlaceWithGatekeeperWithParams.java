a568b76c38d3a464cfc3f7c9251b79a3a26215f6
Juan Carlos González <pentasis.jgg@gmail.com>
2012-12-03 09:36:39 +0100

    GatekeeperWithParams after merging
    This is GatekeeperWithParams implementation after merging

---
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/proxy/PlaceWithGatekeeperWithParams.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.proxy;
+
+/**
+ * Specialized {@link PlaceWithGatekeeper} which uses a {@link GatekeeperWithParams}
+ * and an array of parameters to protect the {@link Place}.
+ *
+ * @author Juan Carlos González
+ */
+public class PlaceWithGatekeeperWithParams extends PlaceImpl {
+
+  private final GatekeeperWithParams gatekeeper;
+  private final String[] params;
+
+  public PlaceWithGatekeeperWithParams(String nameToken, GatekeeperWithParams gatekeeper,
+      String[] params) {
+    super(nameToken);
+    this.gatekeeper = gatekeeper;
+    this.params = params;
+  }
+
+  @Override
+  public boolean canReveal() {
+    return gatekeeper.withParams(params).canReveal();
+  }
+}
