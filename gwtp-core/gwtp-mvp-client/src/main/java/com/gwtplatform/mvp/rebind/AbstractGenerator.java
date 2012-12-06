1da05a47cafa7c0ba60f638f1877b78516a85003
Christian Goudreau <christian.goudreau@arcbees.com>
2012-12-06 14:13:40 -0500

    Added missing information needed by checkstyle to compile
    Former-commit-id: 21065f39bdc39bc940e37b98d40f81439dabbc4b

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/rebind/AbstractGenerator.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/rebind/AbstractGenerator.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.gwtplatform.mvp.rebind;
 
 import com.google.gwt.core.ext.Generator;
@@ -12,7 +28,7 @@ import com.google.gwt.user.rebind.SourceWriter;
 import java.io.PrintWriter;
 
 /**
- * Base generator to use.
+ * Base generator
  */
 public abstract class AbstractGenerator extends Generator {
   private TreeLogger treeLogger;
@@ -78,7 +94,6 @@ public abstract class AbstractGenerator extends Generator {
       throw new UnableToCompleteException();
     }
   }
-
 
   protected void closeDefinition(GeneratorContext generatorContext, PrintWriter printWriter,
       SourceWriter sourceWriter) {
