08c77c807868f4f4888ead0b7bf17a6177029f72
Brandon Donnelson <branflake2267@gmail.com>
2012-12-08 16:42:26 -0800

    added docs

--- gwtp-samples/gwtp-sample-nested/src/test/java/com/gwtplatform/samples/nested/client/SandboxJukitoTest.java
+++ gwtp-samples/gwtp-sample-nested/src/test/java/com/gwtplatform/samples/nested/client/SandboxJukitoTest.java
@@ -23,6 +23,8 @@ import org.jukito.JukitoRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+/**
+ */
 @RunWith(JukitoRunner.class)
 public class SandboxJukitoTest {
     public static class Module extends JukitoModule {
