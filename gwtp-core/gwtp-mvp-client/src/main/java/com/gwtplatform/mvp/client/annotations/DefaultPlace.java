2245356f543970c158ae6c16ffb4fa146c85687f
Simon-Pierre Gingras <simon.pierre.gingras@arcbees.com>
2013-01-29 09:26:58 -0500

    Added DefaultPlace annotation
    Former-commit-id: 9f0e918ccadfc6bb0436b53d00d471cc748f2e6a

---
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/annotations/DefaultPlace.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.annotations;
+
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * This annotation is useful when overriding your {@link com.gwtplatform.mvp.client.proxy.PlaceManager }'s
+ * {@link com.gwtplatform.mvp.client.proxy.PlaceManager#revealCurrentPlace()} method.
+ * <p/>
+ * Use Guice to bind a default name token to this annotation so it can be injected in your {@link com.gwtplatform.mvp
+ * .client.proxy.PlaceManager}'s constructor.
+ */
+@BindingAnnotation
+@Target({FIELD, PARAMETER, METHOD})
+@Retention(RUNTIME)
+public @interface DefaultPlace {
+}
