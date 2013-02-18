724c24e3f07dde5ba032c1ab36eea18610c08b10
Christian Goudreau <christian.goudreau@arcbees.com>
2013-02-18 15:17:28 -0500

    Simplified the templates

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/rebind/velocity/GenerateFormFactorGinjectors.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/rebind/velocity/GenerateFormFactorGinjectors.java
@@ -22,29 +22,21 @@ import com.gwtplatform.mvp.rebind.velocity.ginjectors.FormFactorGinjectorFactory
 import com.gwtplatform.mvp.rebind.velocity.ginjectors.FormFactorGinjectorProviderGenerator;
 
 public class GenerateFormFactorGinjectors {
+    private static final String FORMFACTOR_GINJECTOR_TEMPLATE = "com/gwtplatform/mvp/rebind/FormFactorGinjector.vm";
+    private static final String FORMFACTOR_GINJECTOR_PROVIDER_TEMPLATE
+            = "com/gwtplatform/mvp/rebind/FormFactorGinjectorProvider.vm";
+
     private static final String GINJECTOR_PROVIDER_IMPL_NAME = "GinjectorProvider";
     private static final String GINJECTOR_PROVIDER_TEMPLATE = "com/gwtplatform/mvp/rebind/GinjectorProvider.vm";
 
     private static final String DESKTOP_PROPERTY_NAME = "gin.ginjector.module.desktop";
     private static final String DESKTOP_GINJECTOR_IMPL_NAME = "DesktopGinjector";
-    private static final String DESKTOP_GINJECTOR_PROVIDER_IMPL_NAME = "DesktopGinjectorProvider";
-    private static final String DESKTOP_GINJECTOR_TEMPLATE = "com/gwtplatform/mvp/rebind/DesktopGinjector.vm";
-    private static final String DESKTOP_GINJECTOR_PROVIDER_TEMPLATE
-            = "com/gwtplatform/mvp/rebind/DesktopGinjectorProvider.vm";
 
     private static final String MOBILE_PROPERTY_NAME = "gin.ginjector.module.mobile";
     private static final String MOBILE_GINJECTOR_IMPL_NAME = "MobileGinjector";
-    private static final String MOBILE_GINJECTOR_PROVIDER_IMPL_NAME = "MobileGinjectorProvider";
-    private static final String MOBILE_GINJECTOR_TEMPLATE = "com/gwtplatform/mvp/rebind/MobileGinjector.vm";
-    private static final String MOBILE_GINJECTOR_PROVIDER_TEMPLATE
-            = "com/gwtplatform/mvp/rebind/MobileGinjectorProvider.vm";
 
     private static final String TABLET_PROPERTY_NAME = "gin.ginjector.module.tablet";
     private static final String TABLET_GINJECTOR_IMPL_NAME = "TabletGinjector";
-    private static final String TABLET_GINJECTOR_PROVIDER_IMPL_NAME = "TabletGinjectorProvider";
-    private static final String TABLET_GINJECTOR_TEMPLATE = "com/gwtplatform/mvp/rebind/TabletGinjector.vm";
-    private static final String TABLET_GINJECTOR_PROVIDER_TEMPLATE
-            = "com/gwtplatform/mvp/rebind/TabletGinjectorProvider.vm";
 
     private final FormFactorGinjectorFactory formFactorGinjectorFactory;
 
@@ -74,45 +66,45 @@ public class GenerateFormFactorGinjectors {
     private void generateDesktopFormFactorGenerators() throws Exception {
         FormFactorGinjectorProviderGenerator desktopGinjector
                 = formFactorGinjectorFactory.createGinjector(
-                DESKTOP_GINJECTOR_TEMPLATE,
+                FORMFACTOR_GINJECTOR_TEMPLATE,
                 DESKTOP_PROPERTY_NAME,
                 DESKTOP_GINJECTOR_IMPL_NAME);
         desktopGinjector.generate();
 
         FormFactorGinjectorProviderGenerator desktopGinjectorProvider
                 = formFactorGinjectorFactory.createGinjectorProvider(
-                DESKTOP_GINJECTOR_PROVIDER_TEMPLATE,
-                DESKTOP_GINJECTOR_PROVIDER_IMPL_NAME);
+                FORMFACTOR_GINJECTOR_PROVIDER_TEMPLATE,
+                DESKTOP_GINJECTOR_IMPL_NAME);
         desktopGinjectorProvider.generate();
     }
 
     private void generateMobileFormFactorGenerators() throws Exception {
         FormFactorGinjectorProviderGenerator mobileGinjector
                 = formFactorGinjectorFactory.createGinjector(
-                MOBILE_GINJECTOR_TEMPLATE,
+                FORMFACTOR_GINJECTOR_TEMPLATE,
                 MOBILE_PROPERTY_NAME,
                 MOBILE_GINJECTOR_IMPL_NAME);
         mobileGinjector.generate();
 
         FormFactorGinjectorProviderGenerator mobileGinjectorProvider
                 = formFactorGinjectorFactory.createGinjectorProvider(
-                MOBILE_GINJECTOR_PROVIDER_TEMPLATE,
-                MOBILE_GINJECTOR_PROVIDER_IMPL_NAME);
+                FORMFACTOR_GINJECTOR_PROVIDER_TEMPLATE,
+                MOBILE_GINJECTOR_IMPL_NAME);
         mobileGinjectorProvider.generate();
     }
 
     private void generateTabletFormFactorGenerators() throws Exception {
         FormFactorGinjectorProviderGenerator tabletGinjector
                 = formFactorGinjectorFactory.createGinjector(
-                TABLET_GINJECTOR_TEMPLATE,
+                FORMFACTOR_GINJECTOR_TEMPLATE,
                 TABLET_PROPERTY_NAME,
                 TABLET_GINJECTOR_IMPL_NAME);
         tabletGinjector.generate();
 
         FormFactorGinjectorProviderGenerator tabletGinjectorProvider
                 = formFactorGinjectorFactory.createGinjectorProvider(
-                TABLET_GINJECTOR_PROVIDER_TEMPLATE,
-                TABLET_GINJECTOR_PROVIDER_IMPL_NAME);
+                FORMFACTOR_GINJECTOR_PROVIDER_TEMPLATE,
+                TABLET_GINJECTOR_IMPL_NAME);
         tabletGinjectorProvider.generate();
     }
 }
