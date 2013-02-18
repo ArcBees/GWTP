0ddb0c9c645b602d220d67090301fa600cb19a8e
Brandon Donnelson <branflake2267@gmail.com>
2013-02-18 15:18:56 -0800

    formatting

--- gwtp-core/gwtp-dispatch-shared/src/main/java/com/gwtplatform/dispatch/shared/DefaultSecureActionImpl.java
+++ gwtp-core/gwtp-dispatch-shared/src/main/java/com/gwtplatform/dispatch/shared/DefaultSecureActionImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 ArcBees Inc.
+ * Copyright 2011 ArcBees Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,9 +15,6 @@
  */
 
 package com.gwtplatform.dispatch.shared;
-
-import com.gwtplatform.dispatch.shared.Action;
-import com.gwtplatform.dispatch.shared.Result;
 
 /**
  * Base abstract implementation of
