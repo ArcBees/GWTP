4857bea5c2fa7a05b23be7ac18f95101e9ce1f78
Brandon Donnelson <branflake2267@gmail.com>
2013-02-20 15:39:08 -0800

    Override sop classes for rpc dispatching

---
+++ gwtp-core/gwtp-dispatch-server-guice/src/main/java/com/gwtplatform/dispatch/server/guice/DispatchServiceImplOverrideSop.java
@@ -0,0 +1,55 @@
+package com.gwtplatform.dispatch.server.guice;
+
+import java.util.logging.Logger;
+
+import javax.inject.Singleton;
+
+import com.google.inject.Inject;
+import com.gwtplatform.dispatch.server.Dispatch;
+import com.gwtplatform.dispatch.server.RequestProvider;
+import com.gwtplatform.dispatch.server.guice.overridesop.AbstractDispatchServiceImpl;
+import com.gwtplatform.dispatch.shared.SecurityCookie;
+
+/**
+ * This overrides GWT SOP. This can be used with a remote device setup.
+ * 
+ * Serve this through the Default Servlet Module
+ * serve("/" + ActionImpl.DEFAULT_SERVICE_NAME + "*").with(DispatchServiceImplOverrideSop.class);
+ * 
+ * TODO investigate security for this type of context  
+ * 
+ * This is the server-side implementation of the
+ * {@link com.gwtplatform.dispatch.shared.DispatchService}, for which the
+ * client-side async service is
+ * {@link com.gwtplatform.dispatch.shared.DispatchServiceAsync}.
+ * <p />
+ * This class is closely related to {@link DispatchImpl}, in theory the latter
+ * wouldn't be needed, but we use it to workaround a GWT limitation described in
+ * {@link com.gwtplatform.dispatch.shared.DispatchAsync}.
+ * 
+ * @see com.gwtplatform.dispatch.shared.DispatchAsync
+ * @see com.gwtplatform.dispatch.server.Dispatch
+ * @see com.gwtplatform.dispatch.server.guice.DispatchImpl
+ * @see com.gwtplatform.dispatch.shared.DispatchService
+ * @see com.gwtplatform.dispatch.shared.DispatchServiceAsync
+ * @see com.gwtplatform.dispatch.server.guice.DispatchServiceImpl
+ */
+@Singleton
+public class DispatchServiceImplOverrideSop extends AbstractDispatchServiceImpl {
+    private static final long serialVersionUID = 136176741488585959L;
+
+    @Inject(optional = true)
+    @SecurityCookie
+    protected String securityCookieName;
+
+    @Inject
+    public DispatchServiceImplOverrideSop(final Logger logger, final Dispatch dispatch, 
+            RequestProvider requestProvider) {
+        super(logger, dispatch, requestProvider);
+    }
+
+    @Override
+    public String getSecurityCookieName() {
+        return securityCookieName;
+    }
+}
