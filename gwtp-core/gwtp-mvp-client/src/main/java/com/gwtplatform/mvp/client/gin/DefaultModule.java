bd44d20c5b0a264e1ce53bcad91c7409fa282631
Johannes Barop <johannes.barop@devbliss.com>
2013-03-05 14:11:44 +0100

    Added support for route-like place-tokens

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/gin/DefaultModule.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/gin/DefaultModule.java
@@ -26,6 +26,7 @@ import com.gwtplatform.mvp.client.googleanalytics.GoogleAnalyticsImpl;
 import com.gwtplatform.mvp.client.proxy.DefaultPlaceManager;
 import com.gwtplatform.mvp.client.proxy.ParameterTokenFormatter;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
+import com.gwtplatform.mvp.client.proxy.RouteTokenFormatter;
 import com.gwtplatform.mvp.client.proxy.TokenFormatter;
 
 /**
@@ -36,40 +37,59 @@ import com.gwtplatform.mvp.client.proxy.TokenFormatter;
 public class DefaultModule extends AbstractGinModule {
     private final Class<? extends PlaceManager> placeManagerClass;
 
+    private final Class<? extends TokenFormatter> tokenFormatterClass;
+
     /**
      * When instantiating the module this way be sure to read
      * {@link DefaultPlaceManager}
-     * 
+     *
      * <b>Important!</b> If you use this class, don't forget to bind
      * {@link com.gwtplatform.mvp.client.annotations.DefaultPlace DefaultPlace},
      * {@link com.gwtplatform.mvp.client.annotations.ErrorPlace ErrorPlace} and
      * {@link com.gwtplatform.mvp.client.annotations.UnauthorizedPlace
      * UnauthorizedPlace} to Presenter name tokens in your Gin module.<br/>
-     * 
+     *
      * @see <a href="https://github.com/ArcBees/GWTP/wiki/PlaceManager">See
      *      PlaceManager wiki for more examples</a>
      */
     public DefaultModule() {
-        placeManagerClass = DefaultPlaceManager.class;
+        this(DefaultPlaceManager.class, ParameterTokenFormatter.class);
     }
 
     /**
      * Manually setup a PlaceManager. See {@link DefaultPlaceManager} for more
      * details.<br/>
-     * 
+     *
      * @param placeManagerClass
      *            {@link DefaultPlaceManager} @see <a
      *            href="https://github.com/ArcBees/GWTP/wiki/PlaceManager">See
      *            PlaceManager wiki for more examples</a>
      */
     public DefaultModule(Class<? extends PlaceManager> placeManagerClass) {
+        this(placeManagerClass, ParameterTokenFormatter.class);
+    }
+
+    /**
+     * Manually setup a {@link PlaceManager} and {@link TokenFormatter}.
+     *
+     * <p>
+     * See {@link DefaultPlaceManager}, {@link ParameterTokenFormatter} and {@link RouteTokenFormatter} for more
+     * details.
+     * </p>
+     *
+     * @param placeManagerClass   The {@link PlaceManager} implementation.
+     * @param tokenFormatterClass The {@link TokenFormatter} implementation.
+     */
+    public DefaultModule(Class<? extends PlaceManager> placeManagerClass,
+            Class<? extends TokenFormatter> tokenFormatterClass) {
         this.placeManagerClass = placeManagerClass;
+        this.tokenFormatterClass = tokenFormatterClass;
     }
 
     @Override
     protected void configure() {
         bind(EventBus.class).to(SimpleEventBus.class).in(Singleton.class);
-        bind(TokenFormatter.class).to(ParameterTokenFormatter.class).in(Singleton.class);
+        bind(TokenFormatter.class).to(tokenFormatterClass).in(Singleton.class);
         bind(RootPresenter.class).asEagerSingleton();
         bind(GoogleAnalytics.class).to(GoogleAnalyticsImpl.class).in(Singleton.class);
         bind(PlaceManager.class).to(placeManagerClass).in(Singleton.class);
