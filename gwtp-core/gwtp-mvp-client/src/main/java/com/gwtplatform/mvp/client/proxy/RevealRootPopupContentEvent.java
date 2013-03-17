6b35f97e3e92ea9dd3096aa54b9dd5e55cb64ba6
Björn Moritz <bjoern.moritz@gmail.com>
2013-03-17 16:17:49 +0100

    Fix some checkstyle issues
    Former-commit-id: 7437a94a85ca50395e53ef65c9bf83381fab87a1

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/proxy/RevealRootPopupContentEvent.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/proxy/RevealRootPopupContentEvent.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2011 ArcBees Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -40,6 +40,44 @@ public final class RevealRootPopupContentEvent extends
 
     private static final Type<RevealRootPopupContentHandler> TYPE = new Type<RevealRootPopupContentHandler>();
 
+    /**
+     * Fires a {@link RevealRootPopupContentEvent} into a source that has access
+     * to an {@link com.google.web.bindery.event.shared.EventBus}.
+     * 
+     * @param source
+     *            The source that fires this event ({@link HasHandlers}).
+     * @param content
+     *            The {@link PresenterWidget} with a {@link PopupView} that
+     *            wants to set itself as root content.
+     */
+    public static void fire(final HasHandlers source,
+            final PresenterWidget<? extends PopupView> content) {
+        fire(source, content, true);
+    }
+
+    /**
+     * Fires a {@link RevealRootPopupContentEvent} into a source that has access
+     * to an {@link com.google.web.bindery.event.shared.EventBus}.
+     * 
+     * @param source
+     *            The source that fires this event ({@link HasHandlers}).
+     * @param content
+     *            The {@link PresenterWidget} with a {@link PopupView} that
+     *            wants to set itself as root content.
+     * @param center
+     *            Pass true to center the popup, otherwise its position will not
+     *            be adjusted.
+     */
+    public static void fire(final HasHandlers source,
+            final PresenterWidget<? extends PopupView> content,
+            final boolean center) {
+        source.fireEvent(new RevealRootPopupContentEvent(content, center));
+    }
+
+    public static Type<RevealRootPopupContentHandler> getType() {
+        return TYPE;
+    }
+
     private final PresenterWidget<? extends PopupView> content;
 
     private final boolean center;
@@ -71,42 +109,5 @@ public final class RevealRootPopupContentEvent extends
     @Override
     protected void dispatch(RevealRootPopupContentHandler handler) {
         handler.onRevealRootPopupContent(this);
-    }
-
-    /**
-     * Fires a {@link RevealRootPopupContentEvent} into a source that has access
-     * to an {@link com.google.web.bindery.event.shared.EventBus}.
-     * 
-     * @param source
-     *            The source that fires this event ({@link HasHandlers}).
-     * @param content
-     *            The {@link PresenterWidget} with a {@link PopupView} that
-     *            wants to set itself as root content.
-     */
-    public static void fire(final HasHandlers source,
-            final PresenterWidget<? extends PopupView> content) {
-        fire(source, content, true);
-    }
-
-    /**
-     * Fires a {@link RevealRootPopupContentEvent} into a source that has access
-     * to an {@link com.google.web.bindery.event.shared.EventBus}.
-     * 
-     * @param source
-     *            The source that fires this event ({@link HasHandlers}).
-     * @param content
-     *            The {@link PresenterWidget} with a {@link PopupView} that
-     *            wants to set itself as root content.
-     * @param center
-     *            Pass true to center the popup, otherwise its position will not
-     *            be adjusted.
-     */
-    public static void fire(final HasHandlers source,
-            final PresenterWidget<? extends PopupView> content, boolean center) {
-        source.fireEvent(new RevealRootPopupContentEvent(content, center));
-    }
-
-    public static Type<RevealRootPopupContentHandler> getType() {
-        return TYPE;
     }
 }
