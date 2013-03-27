84cb0f488e8db3fca652e02549e36a3d003ebe39
Brandon Donnelson <branflake2267@gmail.com>
2013-03-26 20:20:31 -0700

    Domain clean up

--- gwtp-carstore/src/main/java/com/arcbees/carsample/shared/dto/ManufacturerRatingDto.java
+++ gwtp-carstore/src/main/java/com/arcbees/carsample/shared/dto/ManufacturerRatingDto.java
@@ -1,14 +1,10 @@
 package com.arcbees.carsample.shared.dto;
 
 public class ManufacturerRatingDto implements Dto {
-
-    private static final long serialVersionUID = 5855002932629231837L;
-
     private String manufacturer;
 
     private Double rating;
 
-    @SuppressWarnings("unused")
     protected ManufacturerRatingDto() {
         // Needed for serialization
     }
@@ -25,5 +21,4 @@ public class ManufacturerRatingDto implements Dto {
     public Double getRating() {
         return rating;
     }
-
 }
