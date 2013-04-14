c420d4b496b1fa179879c580ee24d25680f9a8b2
meriouma <maxime.meriouma@arcbees.com>
2013-04-14 15:26:15 -0400

    Refactor

--- gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/application/widgets/MessageWidgetPage.java
+++ gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/application/widgets/MessageWidgetPage.java
@@ -4,29 +4,33 @@ import javax.inject.Inject;
 
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.gwtplatform.carstore.cucumber.application.BasePage;
-
-import static com.google.gwt.user.client.ui.UIObject.DEBUG_ID_PREFIX;
+import com.gwtplatform.carstore.cucumber.util.FindByDebugId;
 
 public class MessageWidgetPage extends BasePage {
+    @FindByDebugId("successMessage")
+    private WebElement successMessage;
+    @FindByDebugId("errorMessage")
+    private WebElement errorMessage;
+
     @Inject
     public MessageWidgetPage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public Boolean hasSuccessMessage() {
-        try {
-            waitUntilElementIsLoaded(DEBUG_ID_PREFIX + "successMessage");
-            return true;
-        } catch (TimeoutException e) {
-            return false;
-        }
+        return messageIsVisible(successMessage);
     }
 
     public Boolean hasErrorMessage() {
+        return messageIsVisible(errorMessage);
+    }
+
+    private Boolean messageIsVisible(WebElement element) {
         try {
-            waitUntilElementIsLoaded(DEBUG_ID_PREFIX + "errorMessage");
+            waitUntilElementIsVisible(element);
             return true;
         } catch (TimeoutException e) {
             return false;
