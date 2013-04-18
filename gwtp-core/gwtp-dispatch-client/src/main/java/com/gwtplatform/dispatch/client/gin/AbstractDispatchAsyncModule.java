53d7cf7eb2068fc77bf88206329a36601da1e1c1
meriouma <maxime.meriouma@arcbees.com>
2013-04-18 15:28:10 -0400

    Added double-bound notification
    Former-commit-id: 89ed3e73ba76d5e84d999f6bfcbff4808b8ec708

--- gwtp-core/gwtp-dispatch-client/src/main/java/com/gwtplatform/dispatch/client/gin/AbstractDispatchAsyncModule.java
+++ gwtp-core/gwtp-dispatch-client/src/main/java/com/gwtplatform/dispatch/client/gin/AbstractDispatchAsyncModule.java
@@ -61,10 +61,15 @@ import com.gwtplatform.dispatch.shared.SecurityCookieAccessor;
  * @author Brendan Doherty
  */
 public abstract class AbstractDispatchAsyncModule extends AbstractGinModule {
+    private static final Object lock = new Object();
+    private static Boolean alreadyBound = false;
+    private static Class<? extends AbstractDispatchAsyncModule> boundType;
+
     protected final Class<? extends ExceptionHandler> exceptionHandlerType;
     protected final Class<? extends SecurityCookieAccessor> sessionAccessorType;
     protected final Class<? extends ClientActionHandlerRegistry> clientActionHandlerRegistryType;
 
+    /**
     /**
      * A {@link AbstractDispatchAsyncModule} builder.
      * <p/>
@@ -137,8 +142,19 @@ public abstract class AbstractDispatchAsyncModule extends AbstractGinModule {
 
     @Override
     protected void configure() {
-        bind(ExceptionHandler.class).to(exceptionHandlerType);
-        bind(SecurityCookieAccessor.class).to(sessionAccessorType);
-        bind(ClientActionHandlerRegistry.class).to(clientActionHandlerRegistryType).asEagerSingleton();
+        synchronized (lock) {
+            if (alreadyBound) {
+                if (!boundType.equals(getClass())) {
+                    throw new RuntimeException("You are trying to use more than one DispatchAsync implementation. " +
+                            boundType.getName() + " was already installed.");
+                }
+            } else {
+                alreadyBound = true;
+                boundType = getClass();
+                bind(ExceptionHandler.class).to(exceptionHandlerType);
+                bind(SecurityCookieAccessor.class).to(sessionAccessorType);
+                bind(ClientActionHandlerRegistry.class).to(clientActionHandlerRegistryType).asEagerSingleton();
+            }
+        }
     }
 }
