8590ce2a3d5b9dac11690a09d0b8b894981d1d44
meriouma <maxime.meriouma@arcbees.com>
2013-04-18 17:35:42 -0400

    Migrating carstore to REST dispatcher
    Former-commit-id: 111313a53420db9eec5f8b7c3cf8208b389b526e

---
+++ gwtp-carstore/src/main/java/com/gwtplatform/carstore/shared/rest/RestParameter.java
@@ -0,0 +1,7 @@
+package com.gwtplatform.carstore.shared.rest;
+
+public class RestParameter {
+    public static final String ID = "id";
+    public static final String LIMIT = "limit";
+    public static final String OFFSET = "offset";
+}
