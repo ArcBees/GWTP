5934448cdf0e43b4d21e74a384f8540bee9d703a
Maxime Meriouma-Caron <maxime.meriouma@arcbees.com>
2013-04-19 12:24:17 -0400

    CR comments
    Former-commit-id: 9ff2c88e93971d0e015a43fe8393fbe55866aa4e

--- gwtp-carstore/src/main/java/com/gwtplatform/carstore/shared/rest/ResourcesPath.java
+++ gwtp-carstore/src/main/java/com/gwtplatform/carstore/shared/rest/ResourcesPath.java
@@ -6,7 +6,7 @@ public class ResourcesPath {
     public static final String MANUFACTURER = "/manufacturer";
     public static final String COUNT = "/count";
     public static final String RATING = "/rating";
-    public static final String USER = "/user";
+    public static final String SESSION = "/session";
     public static final String LOGOUT = "/logout";
     public static final String CURRENT = "/current";
 }
