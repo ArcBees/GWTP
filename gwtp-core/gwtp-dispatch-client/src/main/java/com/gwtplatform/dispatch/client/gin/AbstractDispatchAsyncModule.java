669c6781b150cc7aaf22b2e1f9c279fbaf2d28ae
meriouma <maxime.meriouma@arcbees.com>
2013-04-27 21:31:14 -0400

    CR Fixes

--- gwtp-core/gwtp-dispatch-client/src/main/java/com/gwtplatform/dispatch/client/gin/AbstractDispatchAsyncModule.java
+++ gwtp-core/gwtp-dispatch-client/src/main/java/com/gwtplatform/dispatch/client/gin/AbstractDispatchAsyncModule.java
@@ -61,7 +61,6 @@ import com.gwtplatform.dispatch.shared.SecurityCookieAccessor;
  * @author Brendan Doherty
  */
 public abstract class AbstractDispatchAsyncModule extends AbstractGinModule {
-    private static final Object lock = new Object();
     private static Boolean alreadyBound = false;
     private static Class<? extends AbstractDispatchAsyncModule> boundType;
 
@@ -70,7 +69,7 @@ public abstract class AbstractDispatchAsyncModule extends AbstractGinModule {
     protected final Class<? extends ClientActionHandlerRegistry> clientActionHandlerRegistryType;
 
     /**
-    /**
+     * /**
      * A {@link AbstractDispatchAsyncModule} builder.
      * <p/>
      * By default, this builder configures the {@link AbstractDispatchAsyncModule} to use
@@ -142,19 +141,17 @@ public abstract class AbstractDispatchAsyncModule extends AbstractGinModule {
 
     @Override
     protected void configure() {
-        synchronized (lock) {
-            if (alreadyBound) {
-                if (!boundType.equals(getClass())) {
-                    throw new RuntimeException("You are trying to use more than one DispatchAsync implementation. " +
-                            boundType.getName() + " was already installed.");
-                }
-            } else {
-                alreadyBound = true;
-                boundType = getClass();
-                bind(ExceptionHandler.class).to(exceptionHandlerType);
-                bind(SecurityCookieAccessor.class).to(sessionAccessorType);
-                bind(ClientActionHandlerRegistry.class).to(clientActionHandlerRegistryType).asEagerSingleton();
+        if (alreadyBound) {
+            if (!boundType.equals(getClass())) {
+                throw new RuntimeException("You are trying to use more than one DispatchAsync implementation. " +
+                        boundType.getName() + " was already installed.");
             }
+        } else {
+            alreadyBound = true;
+            boundType = getClass();
+            bind(ExceptionHandler.class).to(exceptionHandlerType);
+            bind(SecurityCookieAccessor.class).to(sessionAccessorType);
+            bind(ClientActionHandlerRegistry.class).to(clientActionHandlerRegistryType).asEagerSingleton();
         }
     }
 }
