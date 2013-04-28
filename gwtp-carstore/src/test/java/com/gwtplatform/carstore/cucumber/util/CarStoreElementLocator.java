2ceb95a48f280c2d0f0d1fc185f826bc57fe0863
meriouma <maxime.meriouma@arcbees.com>
2013-04-28 13:11:16 -0400

    Refactoring and simplifications
    Former-commit-id: c230eb7f903caa41961a04ab37c40cf4c1f58746

--- gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/util/CarStoreElementLocator.java
+++ gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/util/CarStoreElementLocator.java
@@ -1,31 +1,40 @@
+/*
+ * Copyright (c) 2013 by Zafin Labs, All rights reserved.
+ * This source code, and resulting software, is the confidential and proprietary information
+ * ("Proprietary Information") and is the intellectual property ("Intellectual Property")
+ * of Zafin Labs ("The Company"). You shall not disclose such Proprietary Information and
+ * shall use it only in accordance with the terms and conditions of any and all license
+ * agreements you have entered into with The Company.
+ */
+
 package com.gwtplatform.carstore.cucumber.util;
 
 import java.lang.reflect.Field;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.pagefactory.Annotations;
 import org.openqa.selenium.support.pagefactory.ElementLocator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.google.gwt.user.client.ui.UIObject;
 
 public class CarStoreElementLocator implements ElementLocator {
-    private final SearchContext searchContext;
-    private final boolean shouldCache;
-    private final By by;
+    private final WebDriver webDriver;
+
+    private boolean shouldCache;
+    private By by;
+    private long timeout;
     private WebElement cachedElement;
     private List<WebElement> cachedElementList;
 
-    public CarStoreElementLocator(SearchContext searchContext, Field field) {
-        this.searchContext = searchContext;
+    public CarStoreElementLocator(WebDriver webDriver, Field field) {
+        this.webDriver = webDriver;
 
-        Annotations annotations = new Annotations(field);
-        shouldCache = annotations.isLookupCached();
-
-        FindByDebugId findByDebugId = field.getAnnotation(FindByDebugId.class);
-        by = By.id(UIObject.DEBUG_ID_PREFIX + findByDebugId.value());
+        processAnnotations(field);
     }
 
     @Override
@@ -34,7 +43,7 @@ public class CarStoreElementLocator implements ElementLocator {
             return cachedElement;
         }
 
-        WebElement element = searchContext.findElement(by);
+        WebElement element = doFindElement();
         if (shouldCache) {
             cachedElement = element;
         }
@@ -48,11 +57,52 @@ public class CarStoreElementLocator implements ElementLocator {
             return cachedElementList;
         }
 
-        List<WebElement> elements = searchContext.findElements(by);
+        List<WebElement> elements = doFindElements();
         if (shouldCache) {
             cachedElementList = elements;
         }
 
         return elements;
+    }
+
+    private WebElement doFindElement() {
+        if (timeout > 0) {
+            return new WebDriverWait(webDriver, timeout)
+                    .until(ExpectedConditions.presenceOfElementLocated(by));
+        } else {
+            return webDriver.findElement(by);
+        }
+    }
+
+    private List<WebElement> doFindElements() {
+        if (timeout > 0) {
+            return new WebDriverWait(webDriver, timeout)
+                    .until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
+        } else {
+            return webDriver.findElements(by);
+        }
+    }
+
+    private void processAnnotations(Field field) {
+        Annotations annotations = new Annotations(field);
+        processFindBy(field, annotations);
+        processWaitTimeout(field);
+        shouldCache = annotations.isLookupCached();
+    }
+
+    private void processWaitTimeout(Field field) {
+        LongImplicitWait longImplicitWait = field.getAnnotation(LongImplicitWait.class);
+        if (longImplicitWait != null) {
+            timeout = longImplicitWait.timeout();
+        }
+    }
+
+    private void processFindBy(Field field, Annotations annotations) {
+        FindByDebugId findByDebugId = field.getAnnotation(FindByDebugId.class);
+        if (findByDebugId != null) {
+            by = By.id(UIObject.DEBUG_ID_PREFIX + findByDebugId.value());
+        } else {
+            by = annotations.buildBy();
+        }
     }
 }
--- gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/util/CarStoreElementLocatorFactory.java
+++ gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/util/CarStoreElementLocatorFactory.java
@@ -1,11 +1,11 @@
 package com.gwtplatform.carstore.cucumber.util;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
 import javax.inject.Inject;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.pagefactory.DefaultElementLocator;
 import org.openqa.selenium.support.pagefactory.ElementLocator;
 import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
 
@@ -19,10 +19,6 @@ public class CarStoreElementLocatorFactory implements ElementLocatorFactory {
 
     @Override
     public ElementLocator createLocator(Field field) {
-        if (field.isAnnotationPresent(FindByDebugId.class)) {
-            return new CarStoreElementLocator(driver, field);
-        } else {
-            return new DefaultElementLocator(driver, field);
-        }
+        return new CarStoreElementLocator(driver, field);
     }
 }
