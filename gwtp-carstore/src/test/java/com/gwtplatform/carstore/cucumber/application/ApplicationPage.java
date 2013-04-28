c230eb7f903caa41961a04ab37c40cf4c1f58746
meriouma <maxime.meriouma@arcbees.com>
2013-04-28 13:11:16 -0400

    Refactoring and simplifications

--- gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/application/ApplicationPage.java
+++ gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/application/ApplicationPage.java
@@ -1,18 +1,10 @@
 package com.gwtplatform.carstore.cucumber.application;
 
-import javax.inject.Inject;
-
 import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
 
 import static com.google.gwt.user.client.ui.UIObject.DEBUG_ID_PREFIX;
 
 public class ApplicationPage extends BasePage {
-    @Inject
-    public ApplicationPage(WebDriver webDriver) {
-        super(webDriver);
-    }
-
     public Boolean waitUntilDomIsLoaded(String nameToken) {
         try {
             waitUntilElementIsLoaded(DEBUG_ID_PREFIX + nameToken + "Panel");
--- gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/application/widgets/HeaderWidgetPage.java
+++ gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/application/widgets/HeaderWidgetPage.java
@@ -1,8 +1,5 @@
 package com.gwtplatform.carstore.cucumber.application.widgets;
 
-import javax.inject.Inject;
-
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.gwtplatform.carstore.cucumber.application.BasePage;
@@ -11,11 +8,6 @@ import com.gwtplatform.carstore.cucumber.util.FindByDebugId;
 public class HeaderWidgetPage extends BasePage {
     @FindByDebugId("logout")
     private WebElement logout;
-
-    @Inject
-    public HeaderWidgetPage(WebDriver webDriver) {
-        super(webDriver);
-    }
 
     public void clickOnLogOut() {
         waitUntilElementIsVisible(logout);
