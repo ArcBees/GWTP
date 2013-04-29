de0da83f849a178f05c49ea422c88518dd1a459f
meriouma <maxime.meriouma@arcbees.com>
2013-04-29 03:02:51 -0400

    Styling
    Former-commit-id: 19bbd824757a25da37dd1458e37f3c315a4ab602

--- gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/util/LongImplicitWait.java
+++ gwtp-carstore/src/test/java/com/gwtplatform/carstore/cucumber/util/LongImplicitWait.java
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2013 by Zafin Labs, All rights reserved.
- * This source code, and resulting software, is the confidential and proprietary information
- * ("Proprietary Information") and is the intellectual property ("Intellectual Property")
- * of Zafin Labs ("The Company"). You shall not disclose such Proprietary Information and
- * shall use it only in accordance with the terms and conditions of any and all license
- * agreements you have entered into with The Company.
- */
-
 package com.gwtplatform.carstore.cucumber.util;
 
 import java.lang.annotation.ElementType;
