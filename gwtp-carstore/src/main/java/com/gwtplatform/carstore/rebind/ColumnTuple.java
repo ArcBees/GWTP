7e592abdab5a6bb4c966c3465d2678b1dcbbe05b
Christian Goudreau <christian.goudreau@arcbees.com>
2013-05-02 18:41:45 -0400

    Added columns definition generator

---
+++ gwtp-carstore/src/main/java/com/gwtplatform/carstore/rebind/ColumnTuple.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.carstore.rebind;
+
+import com.google.gwt.core.ext.typeinfo.JMethod;
+import com.gwtplatform.carstore.client.application.rating.ui.Column;
+
+public class ColumnTuple {
+    private JMethod method;
+    private Column column;
+
+    public JMethod getMethod() {
+        return method;
+    }
+
+    public Column getColumn() {
+        return column;
+    }
+
+    public String getCellCanonicalName() {
+        return column.cellType().getCanonicalName();
+    }
+
+    public String getReturnCellCanonicalName() {
+        return column.cellReturnType().getCanonicalName();
+    }
+
+    public String getMethodCapitalizedName() {
+        return method.getName();
+    }
+
+    public static ColumnTuple createFrom(JMethod method) {
+        ColumnTuple columnTuple = new ColumnTuple();
+
+        columnTuple.method = method;
+        columnTuple.column = method.getAnnotation(Column.class);
+
+        return columnTuple;
+    }
+}
---
+++ gwtp-carstore/src/main/java/com/gwtplatform/carstore/rebind/ColumnsInitializerDefinitions.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.carstore.rebind;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.core.ext.typeinfo.JClassType;
+import com.google.gwt.core.ext.typeinfo.JMethod;
+import com.gwtplatform.carstore.client.application.rating.ui.Column;
+import com.gwtplatform.carstore.client.application.rating.ui.ColumnsDefinition;
+
+public class ColumnsInitializerDefinitions {
+    private ColumnsDefinition columnsInitializer;
+    private List<ColumnTuple> columns;
+
+    public List<ColumnTuple> getColumns() {
+        return columns;
+    }
+
+    public String getDtoCanonicalName() {
+        return columnsInitializer.definitionFor().getCanonicalName();
+    }
+
+    public static ColumnsInitializerDefinitions createFrom(JClassType type) {
+        ColumnsInitializerDefinitions columnsInitializerDefinitions = new ColumnsInitializerDefinitions();
+
+        columnsInitializerDefinitions.columnsInitializer = type.getAnnotation(ColumnsDefinition.class);
+
+        List<ColumnTuple> columns = new ArrayList<ColumnTuple>();
+        JMethod[] jMethods = type.getMethods();
+        for (int i = 0; i < jMethods.length; i++) {
+            JMethod method = jMethods[i];
+
+            if (method.isAnnotationPresent(Column.class)) {
+                ColumnTuple columnTuple = ColumnTuple.createFrom(method);
+
+                columns.add(columnTuple);
+            }
+        }
+
+        columnsInitializerDefinitions.columns = columns;
+
+        return columnsInitializerDefinitions;
+    }
+}
