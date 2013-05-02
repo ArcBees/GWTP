1a6f2fde47467168ca7ca9422b4482dfc795f059
Christian Goudreau <christian.goudreau@arcbees.com>
2013-05-02 18:41:45 -0400

    Added columns definition generator

---
+++ gwtp-carstore/src/main/java/com/gwtplatform/carstore/rebind/ColumnsInitializerGenerator.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.carstore.rebind;
+
+import com.google.gwt.core.ext.Generator;
+import com.google.gwt.core.ext.GeneratorContext;
+import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.typeinfo.JClassType;
+import com.google.gwt.core.ext.typeinfo.NotFoundException;
+import com.google.gwt.core.ext.typeinfo.TypeOracle;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class ColumnsInitializerGenerator extends Generator {
+    private Logger logger;
+    private TypeOracle typeOracle;
+    private JClassType type;
+    private Injector injector;
+
+    @Override
+    public String generate(TreeLogger treeLogger, GeneratorContext generatorContext, String typeName)
+            throws UnableToCompleteException {
+        logger = new Logger(treeLogger);
+        typeOracle = generatorContext.getTypeOracle();
+        type = getType(typeName);
+
+        injector = Guice.createInjector(new RebindModule(logger, generatorContext));
+
+        return generateColumnsIntializer();
+    }
+
+    private String generateColumnsIntializer() throws UnableToCompleteException {
+        VelocityColumnsInitializerGenerator velocityColumnsInitializerGenerator =
+                injector.getInstance(VelocityColumnsInitializerGenerator.class);
+        try {
+            return velocityColumnsInitializerGenerator.generate(type);
+        } catch (Exception e) {
+            logger.die(e.getMessage());
+        }
+
+        return null;
+    }
+
+    // Duplicated from GeneratorUtil so that we don't create twice the injector if we don't need it.
+    private JClassType getType(String typeName) throws UnableToCompleteException {
+        try {
+            return typeOracle.getType(typeName);
+        } catch (NotFoundException e) {
+            logger.die("Cannot find " + typeName);
+        }
+
+        return null;
+    }
+}
