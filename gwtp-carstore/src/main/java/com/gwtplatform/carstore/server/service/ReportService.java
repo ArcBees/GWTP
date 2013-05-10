9bb6ffdfc156589aff065ea77f7abb7d065df991
Mathieu Lanouette <mathieu.lanouette@arcbees.com>
2013-05-10 12:07:44 -0400

    Fixed reports data generation

--- gwtp-carstore/src/main/java/com/gwtplatform/carstore/server/service/ReportService.java
+++ gwtp-carstore/src/main/java/com/gwtplatform/carstore/server/service/ReportService.java
@@ -5,44 +5,35 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.google.gwt.dev.util.collect.HashMap;
 import com.gwtplatform.carstore.shared.dto.ManufacturerRatingDto;
+import com.gwtplatform.carstore.shared.dto.RatingDto;
 
 public class ReportService {
     @Inject
     public ReportService() {
     }
 
-    public List<ManufacturerRatingDto> getAverageCarRatingByManufacturer() {
-        
-//        TODO 
-//        Query query = entityManager.createQuery(
-//                "select new map(m.name as manufacturer, c.model as carModel, avg(r.rating) as rating)" +
-//                        " from Manufacturer m" +
-//                        " join m.cars as c" +
-//                        " join c.ratings as r" +
-//                        " group by m.name, c.model");
-//
-//        List resultList = query.getResultList();
-//        HashMap<String, AveragingCounter> averages = new HashMap<String, AveragingCounter>();
-//        for (Object resultObject : resultList) {
-//            HashMap result = (HashMap) resultObject;
-//
-//            String manufacturer = (String) result.get("manufacturer");
-//            Double rating = (Double) result.get("rating");
-//
-//            if (averages.containsKey(manufacturer)) {
-//                averages.get(manufacturer).add(rating);
-//            } else {
-//                averages.put(manufacturer, new AveragingCounter(rating));
-//            }
-//        }
-//
-//        List<ManufacturerRatingDto> results = new ArrayList<ManufacturerRatingDto>(averages.size());
-//        for (String manufacturer : averages.keySet()) {
-//            results.add(new ManufacturerRatingDto(manufacturer, averages.get(manufacturer).average()));
-//        }
+    public List<ManufacturerRatingDto> getAverageCarRatings(List<RatingDto> ratingDtos) {
+        HashMap<String, AveragingCounter> averages = new HashMap<String, AveragingCounter>();
 
-        return new ArrayList<ManufacturerRatingDto>();
+        for (RatingDto ratingDto : ratingDtos) {
+            String manufacturer = ratingDto.getCar().getManufacturer().getName();
+            Double rating = Double.valueOf(ratingDto.getRating());
+
+            if (averages.containsKey(manufacturer)) {
+                averages.get(manufacturer).add(rating);
+            } else {
+                averages.put(manufacturer, new AveragingCounter(rating));
+            }
+        }
+
+        List<ManufacturerRatingDto> results = new ArrayList<ManufacturerRatingDto>(averages.size());
+        for (String manufacturer : averages.keySet()) {
+            results.add(new ManufacturerRatingDto(manufacturer, averages.get(manufacturer).average()));
+        }
+
+        return results;
     }
 
     private class AveragingCounter {
@@ -65,5 +56,4 @@ public class ReportService {
             return count == 0 ? Double.NaN : sum / count;
         }
     }
-
 }
