ec32b7ccf00d270325ba7cd8925b84f6fc600ca7
Mathieu Lanouette <mathieu.lanouette@arcbees.com>
2013-05-11 14:34:39 -0400

    Cosmetic fixes

--- gwtp-carstore/src/main/java/com/gwtplatform/carstore/shared/rest/ResourcesPath.java
+++ gwtp-carstore/src/main/java/com/gwtplatform/carstore/shared/rest/ResourcesPath.java
@@ -6,5 +6,4 @@ public class ResourcesPath {
     public static final String RATING = "/ratings";
     public static final String SESSION = "/session";
     public static final String COUNT = "/count";
-    public static final String REPORT = "/report";
 }
