529b75287ff4645c044303d1bcbb3e8b1fb3cb4a
5inGularity <gamewithlalit@gmail.com>
2013-07-04 20:15:51 +0530

    Using thread-safe map.
    Former-commit-id: 846c8700cb81eb854dbce2ef70ef8f8c46002215

--- gwtp-core/gwtp-dispatch-server-guice/src/main/java/com/gwtplatform/dispatch/server/guice/actionhandlervalidator/LazyActionHandlerValidatorRegistryImpl.java
+++ gwtp-core/gwtp-dispatch-server-guice/src/main/java/com/gwtplatform/dispatch/server/guice/actionhandlervalidator/LazyActionHandlerValidatorRegistryImpl.java
@@ -16,13 +16,14 @@
 
 package com.gwtplatform.dispatch.server.guice.actionhandlervalidator;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import com.google.inject.Injector;
+import com.gwtplatform.dispatch.server.actionhandler.ActionHandler;
 import com.gwtplatform.dispatch.server.actionhandlervalidator.ActionHandlerValidatorClass;
 import com.gwtplatform.dispatch.server.actionhandlervalidator.ActionHandlerValidatorInstance;
 import com.gwtplatform.dispatch.server.actionhandlervalidator.LazyActionHandlerValidatorRegistry;
@@ -50,10 +51,10 @@ public class LazyActionHandlerValidatorRegistryImpl implements
     @Inject
     LazyActionHandlerValidatorRegistryImpl(Injector injector) {
         this.injector = injector;
-        actionHandlerValidatorClasses = new HashMap<Class<? extends Action<?>>, ActionHandlerValidatorClass<? extends
+        actionHandlerValidatorClasses = new ConcurrentHashMap<Class<? extends Action<?>>, ActionHandlerValidatorClass<? extends
                 Action<?>, ? extends Result>>();
-        actionHandlerValidatorInstances = new HashMap<Class<? extends Action<?>>, ActionHandlerValidatorInstance>();
-        validators = new HashMap<Class<? extends ActionValidator>, ActionValidator>();
+        actionHandlerValidatorInstances = new ConcurrentHashMap<Class<? extends Action<?>>, ActionHandlerValidatorInstance>();
+        validators = new ConcurrentHashMap<Class<? extends ActionValidator>, ActionValidator>();
     }
 
     @Override
