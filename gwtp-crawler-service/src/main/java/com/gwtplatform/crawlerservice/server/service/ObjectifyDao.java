92a41dd59957c144ed6f54b369d3729d22c0fbc8
Christopher Viel <viel.christopher@gmail.com>
2013-08-01 14:54:30 -0400

    Fixed NPE in Ofy DAO
    Former-commit-id: 9cc9d315ed2f40a5847be884cc9dd28b3cbb8c8d

--- gwtp-crawler-service/src/main/java/com/gwtplatform/crawlerservice/server/service/ObjectifyDao.java
+++ gwtp-crawler-service/src/main/java/com/gwtplatform/crawlerservice/server/service/ObjectifyDao.java
@@ -16,35 +16,33 @@
 
 package com.gwtplatform.crawlerservice.server.service;
 
-import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.Map;
 
-import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.cmd.LoadType;
 import com.googlecode.objectify.cmd.Query;
-import com.gwtplatform.crawlerservice.server.objectify.Ofy;
-import com.gwtplatform.crawlerservice.server.objectify.OfyFactory;
 
 /**
  * Generic DAO for use with Objectify.
  *
- * @param <T>
- * @author David M. Chandler
- * @author Brandon Donnelson
+ * @param <T> The type of the entity handled by this DAO.
  */
-public class ObjectifyDao<T> {
-    static final int BAD_MODIFIERS = Modifier.FINAL | Modifier.STATIC | Modifier.TRANSIENT;
+public abstract class ObjectifyDao<T> {
+    protected final Class<T> clazz;
 
-    protected Class<T> clazz;
-
-    private OfyFactory ofyFactory;
-    private Ofy lazyOfy;
+    private Objectify lazyOfy;
 
     @SuppressWarnings("unchecked")
     public ObjectifyDao() {
         clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+    public LoadType<T> load() {
+        return ofy().load().type(clazz);
     }
 
     public Key<T> put(T entity) {
@@ -71,12 +69,12 @@ public class ObjectifyDao<T> {
         ofy().delete().keys(keys);
     }
 
-    public T get(Long id) throws EntityNotFoundException {
-        return ofy().get(this.clazz, id);
+    public T get(Long id) {
+        return load().id(id).now();
     }
 
-    public T get(Key<T> key) throws EntityNotFoundException {
-        return ofy().get(key);
+    public T get(Key<T> key) {
+        return ofy().load().key(key).now();
     }
 
     public Map<Key<T>, T> get(Iterable<Key<T>> keys) {
@@ -84,41 +82,41 @@ public class ObjectifyDao<T> {
     }
 
     public List<T> listAll(int start, int length) {
-        Query<T> q = ofy().query(clazz).offset(start).limit(length);
+        Query<T> q = load().offset(start).limit(length);
         return q.list();
     }
 
     public int countAll() {
-        return ofy().query(clazz).count();
+        return load().count();
     }
 
     public List<T> listByProperty(String propName, Object propValue) {
-        Query<T> q = ofy().query(clazz);
+        Query<T> q = load();
         q.filter(propName, propValue);
         return q.list();
     }
 
     public List<Key<T>> listKeysByProperty(String propName, Object propValue) {
-        Query<T> q = ofy().query(clazz);
+        Query<T> q = load();
         q.filter(propName, propValue);
         return q.keys().list();
     }
 
     public Key<T> getKey(Long id) {
-        return Key.create(this.clazz, id);
+        return Key.create(clazz, id);
     }
 
     public List<T> listChildren(Object parent) {
-        return ofy().query(clazz).ancestor(parent).list();
+        return load().ancestor(parent).list();
     }
 
     public List<Key<T>> listChildKeys(Object parent) {
-        return ofy().query(clazz).ancestor(parent).keys().list();
+        return load().ancestor(parent).keys().list();
     }
 
-    protected Ofy ofy() {
+    protected Objectify ofy() {
         if (lazyOfy == null) {
-            lazyOfy = ofyFactory.begin();
+            lazyOfy = ObjectifyService.ofy( );
         }
         return lazyOfy;
     }
