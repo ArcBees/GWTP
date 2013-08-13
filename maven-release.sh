d207517fbacaf2a64badc2acc972d31df7ef693e
branflake2267@gmail.com <branflake2267@gmail.com>
2013-08-12 18:48:59 -0700

    Updating maven-release.sh steps.
    Former-commit-id: 5cb35315df15d3e5ffa849cd7223d0f612a74ea1

--- maven-release.sh
+++ maven-release.sh
@@ -1,15 +1,15 @@
 #/bin/sh
 # maven release helper
-# run link 'sh maven-release.sh gpg github.username github.password' replacing the 3 parameters
+# run link 'sh maven-release.sh xxxgpg-passxxx' replacing the 3 parameters
 
 echo "Started"
 
-#TODO add to os enviroment, means I have to reboot and hadn't had time to do it.
-export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+#Be sure Java home is set to 1.7
+#export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 
 mvn clean deploy -Prelease -DskipTests
 
-mvn release:clean release:prepare --batch-mode -Dgpg.passphrase=$1 -Dgithub.username=$2 -Dgithub.password=$3
+mvn release:clean release:prepare --batch-mode -Dgpg.passphrase=$1
 
 mvn release:perform -Dgpg.passphrase=$1
 
