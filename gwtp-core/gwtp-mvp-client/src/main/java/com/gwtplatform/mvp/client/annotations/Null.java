af80abfeec311b1b4312a7c7a05087562f9017b6
Danilo Reinert <daniloreinert@gmail.com>
2013-09-18 23:44:09 -0300

    Added placeholder class refering to null java keyword

---
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/annotations/Null.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.annotations;
+
+/**
+ * The {@code Null} class is an uninstantiable placeholder class to hold a
+ * reference to the {@code Class} object representing the Java keyword
+ * null.
+ *
+ * @author Danilo Reinert
+ */
+public final class Null {
+
+    /*
+     * The Null class cannot be instantiated.
+     */
+    private Null() { }
+}
