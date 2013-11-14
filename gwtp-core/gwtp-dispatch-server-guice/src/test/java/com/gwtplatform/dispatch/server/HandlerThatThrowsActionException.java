62bc4eef73f54af5e929f056162c5a0c33329fdc
spg <simon.pierre.gingras@arcbees.com>
2013-11-14 10:26:08 -0500

    CR Fix

--- gwtp-core/gwtp-dispatch-server-guice/src/test/java/com/gwtplatform/dispatch/server/HandlerThatThrowsActionException.java
+++ gwtp-core/gwtp-dispatch-server-guice/src/test/java/com/gwtplatform/dispatch/server/HandlerThatThrowsActionException.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.gwtplatform.dispatch.server;
 
 import javax.inject.Inject;
@@ -14,7 +30,7 @@ public class HandlerThatThrowsActionException extends AbstractActionHandler<Some
 
     @Override
     public NoResult execute(SomeAction action, ExecutionContext context) throws ActionException {
-        throw new ActionExceptionThrownByHandler();
+        throw new ActionExceptionThrownByHandler(new Exception());
     }
 
     @Override
