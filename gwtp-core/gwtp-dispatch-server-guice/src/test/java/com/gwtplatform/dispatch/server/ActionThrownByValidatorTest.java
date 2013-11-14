ba8b69a2b15c64198d76960adbee95ac8d51affb
spg <simon.pierre.gingras@arcbees.com>
2013-11-14 10:26:08 -0500

    CR Fix
    Former-commit-id: 895a5bd9c79204440e3dad81eb64088e54bc4975

--- gwtp-core/gwtp-dispatch-server-guice/src/test/java/com/gwtplatform/dispatch/server/ActionThrownByValidatorTest.java
+++ gwtp-core/gwtp-dispatch-server-guice/src/test/java/com/gwtplatform/dispatch/server/ActionThrownByValidatorTest.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.gwtplatform.dispatch.server;
 
 import javax.inject.Inject;
@@ -15,7 +31,6 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-
 
 @RunWith(JukitoRunner.class)
 public class ActionThrownByValidatorTest {
@@ -37,6 +52,7 @@ public class ActionThrownByValidatorTest {
         } catch (ActionException e) {
             assertThat(e, instanceOf(ActionExceptionThrownByValidator.class));
             assertEquals(0, e.getStackTrace().length);
+            assertEquals(0, e.getCause().getStackTrace().length);
         }
     }
 }
