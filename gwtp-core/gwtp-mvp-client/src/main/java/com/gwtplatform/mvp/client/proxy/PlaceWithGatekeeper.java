1bc77496c4684b583176bb56445eae8a07e3583d
David Ignjić <ignjic@gmail.com>
2013-11-20 09:40:00 +0100

    Map More than one name token to presenter
    Change annotation NameToken value to array of name tokens. Change
    PlaceImpl. For tab content proxy just use first nametoken.
    
    
    Former-commit-id: c02b44e495decbbbde47b1b503544b658cbd8150

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/proxy/PlaceWithGatekeeper.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/proxy/PlaceWithGatekeeper.java
@@ -24,7 +24,11 @@ public class PlaceWithGatekeeper extends PlaceImpl {
     private final Gatekeeper gatekeeper;
 
     public PlaceWithGatekeeper(String nameToken, Gatekeeper gatekeeper) {
-        super(nameToken);
+        this(new String[] { nameToken }, gatekeeper);
+    }
+
+    public PlaceWithGatekeeper(String[] nameTokens, Gatekeeper gatekeeper) {
+        super(nameTokens);
         this.gatekeeper = gatekeeper;
     }
 
