c6b6ff0c72b2d5a69980c7c9243595a9f86efdc3
Christopher Viel <viel.christopher@gmail.com>
2013-11-23 15:22:17 -0500

    Fixed merge issues.

--- gwtp-core/gwtp-dispatch-rpc-server-guice/src/test/java/com/gwtplatform/dispatch/rpc/server/ActionExceptionThrownByHandler.java
+++ gwtp-core/gwtp-dispatch-rpc-server-guice/src/test/java/com/gwtplatform/dispatch/rpc/server/ActionExceptionThrownByHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011 ArcBees Inc.
+ * Copyright 2013 ArcBees Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -14,12 +14,12 @@
  * the License.
  */
 
-package com.gwtplatform.dispatch.server;
+package com.gwtplatform.dispatch.rpc.server;
 
 import com.gwtplatform.dispatch.shared.ActionException;
 
-public class ActionExceptionThrownByValidator extends ActionException {
-    public ActionExceptionThrownByValidator(Throwable cause) {
+public class ActionExceptionThrownByHandler extends ActionException {
+    public ActionExceptionThrownByHandler(Throwable cause) {
         super("", cause);
     }
 }
