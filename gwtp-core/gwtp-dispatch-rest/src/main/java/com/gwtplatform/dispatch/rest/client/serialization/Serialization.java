a80b2007a4229cabe9af7de68bc985b7c52359e5
Christopher Viel <viel.christopher@gmail.com>
2013-12-01 11:17:48 -0500

    Added some javadoc to the serialization code.

--- gwtp-core/gwtp-dispatch-rest/src/main/java/com/gwtplatform/dispatch/rest/client/serialization/Serialization.java
+++ gwtp-core/gwtp-dispatch-rest/src/main/java/com/gwtplatform/dispatch/rest/client/serialization/Serialization.java
@@ -16,12 +16,49 @@
 
 package com.gwtplatform.dispatch.rest.client.serialization;
 
+/**
+ * Offers basic serialization methods. The results depends on the implementation used.
+ *
+ * @see JsonSerialization
+ */
 public interface Serialization {
+    // Storing the parameterized type as a string works for limited use cases.
+    // Guice's TypeLiteral has very limited uses client-side (.equals() doesn't work).
+    // We may create a custom TypeLiteral that fits our need when it is required.
+
+    /**
+     * Verify if the given parameterized type can be serialized.
+     *
+     * @param parameterizedType the parameterized type.
+     * @return {@code true} if <code>parameterizedType</code> can be serialized, otherwise {@code false}.
+     */
     boolean canSerialize(String parameterizedType);
 
-    <T> String serialize(T o, String parameterizedType);
+    /**
+     * Serializes the object as a type represented by <code>parameterizedType</code>.
+     *
+     * @param object            the object to serialized.
+     * @param parameterizedType the parameterized type of the object to serialize.
+     * @param <T>               the type of the object.
+     * @return the serialized object as a String.
+     */
+    <T> String serialize(T object, String parameterizedType);
 
+    /**
+     * Verify if the given parameterized type can be deserialized.
+     *
+     * @param parameterizedType the parameterized type.
+     * @return {@code true} if <code>parameterizedType</code> can be deserialized, otherwise {@code false}.
+     */
     boolean canDeserialize(String parameterizedType);
 
-    <T> T deserialize(String json, String parameterizedType);
+    /**
+     * Deserializes the object to the type represented by <code>parameterizedType</code>.
+     *
+     * @param serializedObject  the String representing the serialized object.
+     * @param parameterizedType the parameterized type of the expected return type.
+     * @param <T>               the return type.
+     * @return The deserialized object of type <code>T</code>.
+     */
+    <T> T deserialize(String serializedObject, String parameterizedType);
 }
