0cce1194383a02d4722b343014590517f0536114
Christopher Viel <viel.christopher@gmail.com>
2014-02-03 16:01:58 -0500

    javadoc clarifications

--- gwtp-core/gwtp-dispatch-rest/src/main/java/com/gwtplatform/dispatch/rest/client/NoXsrfHeader.java
+++ gwtp-core/gwtp-dispatch-rest/src/main/java/com/gwtplatform/dispatch/rest/client/NoXsrfHeader.java
@@ -22,10 +22,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation used to disable X-CSRF header on REST actions.
- * If used on a resource type, this will disable the X-CSRF header for all descendant actions.
- * If used on a sub-resource method, this will disable the X-CSRF header for all descendant actions.
- * If used on an action method, this will disable the X-CSRF header for that action.
+ * Annotation used to disable the XSRF header on REST actions and services.
+ * If used on a resource type, this will disable the XSRF header for all descendant actions.
+ * If used on a sub-resource method, this will disable the XSRF header for all descendant actions.
+ * If used on an action method, this will disable the XSRF header for that action.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
