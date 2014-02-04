1800e21c7e4d91a4b81077d7173728a2756609c9
Christopher Viel <viel.christopher@gmail.com>
2014-02-04 13:25:04 -0500

    Extracted rest module builder

---
+++ gwtp-core/gwtp-dispatch-rest/src/main/java/com/gwtplatform/dispatch/rest/client/gin/RestDispatchAsyncModuleBuilder.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2014 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.dispatch.rest.client.gin;
+
+import com.gwtplatform.dispatch.client.gin.AbstractDispatchAsyncModule;
+import com.gwtplatform.dispatch.rest.client.serialization.JsonSerialization;
+import com.gwtplatform.dispatch.rest.client.serialization.Serialization;
+
+/**
+ * A {@link RestDispatchAsyncModule} builder.
+ * <p/>
+ * The possible configurations are:
+ * <ul>
+ * <li>All configuration methods listed by {@link AbstractDispatchAsyncModule.Builder}</li>
+ * <li>{@link #xsrfTokenHeaderName(String) XSRF Token Header Name}:
+ * A {@link com.gwtplatform.dispatch.rest.client.XSRFHeaderName}.
+ * The default value is {@link RestDispatchAsyncModule#DEFAULT_XSRF_NAME}.</li>
+ * <li>{@link #serialization(Class) Serialization Implementation}: A {@link Serialization} implementation.
+ * The default is {@link JsonSerialization}.</li>
+ * <li>{@link #requestTimeout(int) Request timeout}: The number of milliseconds to wait for a request to complete.
+ * The default value is 0 (no timeout).</li>
+ * </ul>
+ */
+public class RestDispatchAsyncModuleBuilder extends AbstractDispatchAsyncModule.Builder {
+    String xsrfTokenHeaderName = RestDispatchAsyncModule.DEFAULT_XSRF_NAME;
+    Class<? extends Serialization> serializationClass = JsonSerialization.class;
+    int requestTimeoutMs = 0;
+
+    /**
+     * Specify the XSRF token header name.
+     *
+     * @deprecated See {@link #xsrfTokenHeaderName(String)}
+     */
+    @Deprecated
+    public RestDispatchAsyncModuleBuilder xcsrfTokenHeaderName(String xsrfTokenHeaderName) {
+        this.xsrfTokenHeaderName = xsrfTokenHeaderName;
+        return this;
+    }
+
+    /**
+     * Specify the XSRF token header name.
+     *
+     * @param xsrfTokenHeaderName The XSRF token header name.
+     * @return this {@link RestDispatchAsyncModuleBuilder builder} object.
+     */
+    public RestDispatchAsyncModuleBuilder xsrfTokenHeaderName(String xsrfTokenHeaderName) {
+        this.xsrfTokenHeaderName = xsrfTokenHeaderName;
+        return this;
+    }
+
+    /**
+     * Specify the serialization implementation to use.
+     * Default is {@link JsonSerialization}.
+     *
+     * @param serializationClass The {@link Serialization} implementation to use.
+     * @return this {@link RestDispatchAsyncModuleBuilder builder} object.
+     */
+    public RestDispatchAsyncModuleBuilder serialization(Class<? extends Serialization> serializationClass) {
+        this.serializationClass = serializationClass;
+        return this;
+    }
+
+    /**
+     * Specify the number of milliseconds to wait for a request to complete. If the timeout is reached,
+     * {@link com.google.gwt.user.client.rpc.AsyncCallback#onFailure(Throwable) AsyncCallback#onFailure(Throwable)}
+     * will be called.
+     * Default is <code>0</code>: no timeout.
+     *
+     * @param timeoutMs The maximum time to wait, in milliseconds, or {@code 0} for no timeout.
+     * @return this {@link RestDispatchAsyncModuleBuilder builder} object.
+     */
+    public RestDispatchAsyncModuleBuilder requestTimeout(int timeoutMs) {
+        this.requestTimeoutMs = timeoutMs;
+        return this;
+    }
+
+    @Override
+    public RestDispatchAsyncModule build() {
+        return new RestDispatchAsyncModule(this);
+    }
+}
