4312d3ddc522af275b87e89a6097302988f10ed0
Christopher Viel <viel.christopher@gmail.com>
2014-02-05 15:51:16 -0500

    Code style
    Former-commit-id: 72eb5991544a42ade859977be0fad0a89c2fe986

--- gwtp-core/gwtp-dispatch-rest/src/test/java/com/gwtplatform/dispatch/rest/client/DefaultRestRequestBuilderFactoryTest.java
+++ gwtp-core/gwtp-dispatch-rest/src/test/java/com/gwtplatform/dispatch/rest/client/DefaultRestRequestBuilderFactoryTest.java
@@ -50,10 +50,11 @@ public class DefaultRestRequestBuilderFactoryTest {
         @Override
         protected void configureTest() {
             bindConstant().annotatedWith(RestApplicationPath.class).to(APPLICATION_PATH);
-            bindConstant().annotatedWith(XSRFHeaderName.class).to(XSRF_HEADER_NAME);
+            bindConstant().annotatedWith(XsrfHeaderNameRename.class).to(XSRF_HEADER_NAME);
             bindConstant().annotatedWith(RequestTimeout.class).to(TIMEOUT);
 
             forceMock(HttpRequestBuilderFactory.class);
+            forceMock(RequestBuilder.class);
         }
     }
 
@@ -77,13 +78,11 @@ public class DefaultRestRequestBuilderFactoryTest {
     private DefaultRestRequestBuilderFactory factory;
     @Inject
     private HttpRequestBuilderFactory httpRequestBuilderFactory;
-
+    @Inject
     private RequestBuilder requestBuilder;
 
     @Before
     public void setUp(HttpRequestBuilderFactory httpRequestBuilderFactory, UrlUtils urlUtils) {
-        requestBuilder = mock(RequestBuilder.class);
-
         given(httpRequestBuilderFactory.create(any(Method.class), anyString())).willReturn(requestBuilder);
 
         given(urlUtils.encodeQueryString(DECODED_VALUE_1)).willReturn(ENCODED_VALUE_1);
