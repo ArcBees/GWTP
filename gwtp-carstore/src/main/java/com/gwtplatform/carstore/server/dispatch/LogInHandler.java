79d3bc73a6a03353ec16641f4bbf5162533b0c47
Maxime Meriouma-Caron <maxime.meriouma@arcbees.com>
2014-07-27 17:08:30 -0400

    CR Fixes

--- gwtp-carstore/src/main/java/com/gwtplatform/carstore/server/dispatch/LogInHandler.java
+++ gwtp-carstore/src/main/java/com/gwtplatform/carstore/server/dispatch/LogInHandler.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.gwtplatform.carstore.server.dispatch;
 
 import java.util.logging.Logger;
@@ -18,11 +34,12 @@ import com.gwtplatform.dispatch.shared.ActionException;
 public class LogInHandler extends AbstractActionHandler<LogInAction, LogInResult> {
     private final Authenticator authenticator;
     private final UserSessionDao loginCookieDao;
+    private final Logger logger;
+    
     private boolean isLoggedIn;
-    private Logger logger;
 
     @Inject
-    public LogInHandler(Logger logger, Authenticator authenticator, UserSessionDao loginCookieDao) {
+    LogInHandler(Logger logger, Authenticator authenticator, UserSessionDao loginCookieDao) {
         super(LogInAction.class);
 
         this.logger = logger;
