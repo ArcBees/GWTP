42a7df563265b779c6c941a93624e19a4eb1c7ac
Richard Wallis <rdwallis@gmail.com>
2014-10-09 18:10:55 +0200

    clip relative to widget popups to window

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/view/RelativeToWidgetPopupPositioner.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/view/RelativeToWidgetPopupPositioner.java
@@ -24,15 +24,32 @@ import com.google.gwt.user.client.ui.Widget;
  *  Positions the popup relative to a widget.
  */
 public class RelativeToWidgetPopupPositioner extends PopupPositioner {
-    private IsWidget widget;
+    private final Widget widget;
+    private final boolean clipToWindow;
 
+    /**
+     * @param widget - the widget relative to which the popup will be shown.
+     * By default the popup will be clipped to left edge of the screen if there
+     * is not enough space for it.
+     * call {@link #RelativeToWidgetPopupPositioner(IsWidget, boolean)} if you
+     * want the popup to always be shown relative to the widget and the
+     * page to be expanded if there is no space.
+     */
     public RelativeToWidgetPopupPositioner(IsWidget widget) {
-        super();
-        this.widget = widget;
+        this(widget, true);
     }
 
     /**
-     * Copied from gwt popupPanel;
+     * @param widget - the widget relative to which the popup will be shown.
+     * @param clipToWindow - set to true and the popup will be positioned on the left edge
+     * of the screen if it cannot fit.
+     */
+    public RelativeToWidgetPopupPositioner(IsWidget widget, boolean clipToWindow) {
+        this.widget = widget.asWidget();
+        this.clipToWindow = clipToWindow;
+    }
+
+    /**
      * Positions the popup, called after the offset width and height of the popup
      * are known.
      *
@@ -41,119 +58,64 @@ public class RelativeToWidgetPopupPositioner extends PopupPositioner {
      */
     @Override
     public PopupPosition getPopupPosition(int offsetWidth, int offsetHeight) {
-        Widget relativeObject = widget.asWidget();
-
-        // Calculate left position for the popup. The computation for
-        // the left position is bidi-sensitive.
-
-        int textBoxOffsetWidth = relativeObject.getOffsetWidth();
-
-        // Compute the difference between the popup's width and the
-        // textbox's width
-        int offsetWidthDiff = offsetWidth - textBoxOffsetWidth;
-
+        int offsetWidthDiff = offsetWidth - widget.getOffsetWidth();
         int left;
 
         if (LocaleInfo.getCurrentLocale().isRTL()) { // RTL case
 
-            int textBoxAbsoluteLeft = relativeObject.getAbsoluteLeft();
+            left = widget.getAbsoluteLeft() - offsetWidthDiff;
 
-            // Right-align the popup. Note that this computation is
-            // valid in the case where offsetWidthDiff is negative.
-            left = textBoxAbsoluteLeft - offsetWidthDiff;
-
-            // If the suggestion popup is not as wide as the text box, always
-            // align to the right edge of the text box. Otherwise, figure out whether
-            // to right-align or left-align the popup.
             if (offsetWidthDiff > 0) {
-
-                // Make sure scrolling is taken into account, since
-                // box.getAbsoluteLeft() takes scrolling into account.
                 int windowRight = Window.getClientWidth() + Window.getScrollLeft();
                 int windowLeft = Window.getScrollLeft();
 
-                // Compute the left value for the right edge of the textbox
-                int textBoxLeftValForRightEdge = textBoxAbsoluteLeft + textBoxOffsetWidth;
+                int rightEdge = widget.getAbsoluteLeft() + widget.getOffsetWidth();
 
-                // Distance from the right edge of the text box to the right edge
-                // of the window
-                int distanceToWindowRight = windowRight - textBoxLeftValForRightEdge;
+                int distanceToWindowRight = windowRight - rightEdge;
+                int distanceFromWindowLeft = rightEdge - windowLeft;
 
-                // Distance from the right edge of the text box to the left edge of the
-                // window
-                int distanceFromWindowLeft = textBoxLeftValForRightEdge - windowLeft;
-
-                // If there is not enough space for the overflow of the popup's
-                // width to the right of the text box and there IS enough space for the
-                // overflow to the right of the text box, then left-align the popup.
-                // However, if there is not enough space on either side, stick with
-                // right-alignment.
-                if (distanceFromWindowLeft < offsetWidth && distanceToWindowRight >= offsetWidthDiff) {
-                    // Align with the left edge of the text box.
-                    left = textBoxAbsoluteLeft;
+                if (distanceFromWindowLeft < offsetWidth &&
+                        (clipToWindow || distanceToWindowRight >= offsetWidthDiff)) {
+                     left = widget.getAbsoluteLeft();
+                     if (clipToWindow) {
+                         left = Math.min(windowRight - offsetWidth, left);
+                         left = Math.max(0, left);
+                     }
                 }
             }
-        } else { // LTR case
+        } else {
+           left = widget.getAbsoluteLeft();
 
-            // Left-align the popup.
-            left = relativeObject.getAbsoluteLeft();
-
-            // If the suggestion popup is not as wide as the text box, always align to
-            // the left edge of the text box. Otherwise, figure out whether to
-            // left-align or right-align the popup.
             if (offsetWidthDiff > 0) {
-                // Make sure scrolling is taken into account, since
-                // box.getAbsoluteLeft() takes scrolling into account.
                 int windowRight = Window.getClientWidth() + Window.getScrollLeft();
                 int windowLeft = Window.getScrollLeft();
 
-                // Distance from the left edge of the text box to the right edge
-                // of the window
                 int distanceToWindowRight = windowRight - left;
-
-                // Distance from the left edge of the text box to the left edge of the
-                // window
                 int distanceFromWindowLeft = left - windowLeft;
 
-                // If there is not enough space for the overflow of the popup's
-                // width to the right of hte text box, and there IS enough space for the
-                // overflow to the left of the text box, then right-align the popup.
-                // However, if there is not enough space on either side, then stick with
-                // left-alignment.
-                if (distanceToWindowRight < offsetWidth && distanceFromWindowLeft >= offsetWidthDiff) {
-                    // Align with the right edge of the text box.
+                if (distanceToWindowRight < offsetWidth &&
+                        (clipToWindow || distanceFromWindowLeft >= offsetWidthDiff)) {
                     left -= offsetWidthDiff;
+                    if (clipToWindow) {
+                        left = Math.max(0, left);
+                    }
                 }
             }
         }
 
-        // Calculate top position for the popup
+        int top = widget.getAbsoluteTop();
 
-        int top = relativeObject.getAbsoluteTop();
-
-        // Make sure scrolling is taken into account, since
-        // box.getAbsoluteTop() takes scrolling into account.
         int windowTop = Window.getScrollTop();
         int windowBottom = Window.getScrollTop() + Window.getClientHeight();
 
-        // Distance from the top edge of the window to the top edge of the
-        // text box
         int distanceFromWindowTop = top - windowTop;
 
-        // Distance from the bottom edge of the window to the bottom edge of
-        // the text box
-        int distanceToWindowBottom = windowBottom - (top + relativeObject.getOffsetHeight());
+        int distanceToWindowBottom = windowBottom - (top + widget.getOffsetHeight());
 
-        // If there is not enough space for the popup's height below the text
-        // box and there IS enough space for the popup's height above the text
-        // box, then then position the popup above the text box. However, if there
-        // is not enough space on either side, then stick with displaying the
-        // popup below the text box.
         if (distanceToWindowBottom < offsetHeight && distanceFromWindowTop >= offsetHeight) {
             top -= offsetHeight;
         } else {
-            // Position above the text box
-            top += relativeObject.getOffsetHeight();
+            top += widget.getOffsetHeight();
         }
         return new PopupPosition(left, top);
     }
