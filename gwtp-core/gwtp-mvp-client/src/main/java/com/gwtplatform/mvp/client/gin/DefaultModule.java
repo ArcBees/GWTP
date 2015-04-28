db1f2f3df72d58d0c22f6d01df8ba4526c3e4bf9
Christopher Viel <viel.christopher@gmail.com>
2015-04-28 15:01:15 -0400

    Add name token configuration to DefaultModule builder

--- gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/gin/DefaultModule.java
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/gin/DefaultModule.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2011 ArcBees Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -16,6 +16,8 @@
 
 package com.gwtplatform.mvp.client.gin;
 
+import java.lang.annotation.Annotation;
+
 import javax.inject.Singleton;
 
 import com.google.gwt.inject.client.AbstractGinModule;
@@ -23,28 +25,28 @@ import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.event.shared.SimpleEventBus;
 import com.gwtplatform.common.client.CommonGinModule;
 import com.gwtplatform.mvp.client.RootPresenter;
+import com.gwtplatform.mvp.client.annotations.DefaultPlace;
+import com.gwtplatform.mvp.client.annotations.ErrorPlace;
+import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
 import com.gwtplatform.mvp.client.googleanalytics.GoogleAnalytics;
 import com.gwtplatform.mvp.client.googleanalytics.GoogleAnalyticsImpl;
 import com.gwtplatform.mvp.client.proxy.DefaultPlaceManager;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.shared.proxy.ParameterTokenFormatter;
-import com.gwtplatform.mvp.shared.proxy.RouteTokenFormatter;
 import com.gwtplatform.mvp.shared.proxy.TokenFormatter;
 
 /**
- * Module with default GWTP bindings. You can
- * {@code install(new DefaultModule(MyPlaceManager.class))} instead of manually
- * binding the different classes to their default implementation.
+ * Module with default GWTP bindings. You can use {@link DefaultModule.Builder} to configure each required bindings.
  */
 public class DefaultModule extends AbstractGinModule {
-    private final Class<? extends PlaceManager> placeManagerClass;
-
-    private final Class<? extends TokenFormatter> tokenFormatterClass;
-
     /**
-     * A DefaultModule builder.
+     * A builder to configure the bindings used by {@link DefaultModule}.
      */
     public static class Builder {
+        private String defaultPlace;
+        private String errorPlace;
+        private String unauthorizedPlace;
+        private Class<? extends EventBus> eventBusClass = SimpleEventBus.class;
         private Class<? extends PlaceManager> placeManagerClass = DefaultPlaceManager.class;
         private Class<? extends TokenFormatter> tokenFormatterClass = ParameterTokenFormatter.class;
 
@@ -61,77 +63,107 @@ public class DefaultModule extends AbstractGinModule {
             return this;
         }
 
+        public Builder eventBus(Class<? extends EventBus> eventBusClass) {
+            this.eventBusClass = eventBusClass;
+            return this;
+        }
+
+        /**
+         * Bind a value to the constant annotated with {@link DefaultPlace @DefaultPlace}. Assigning a value to this
+         * constant is mandatory if using {@link DefaultPlaceManager}. As a convenience, you may have your custom {@link
+         * PlaceManager} reuse this constant.
+         */
+        public Builder defaultPlace(String defaultPlace) {
+            this.defaultPlace = defaultPlace;
+            return this;
+        }
+
+        /**
+         * Bind a value to the constant annotated with {@link ErrorPlace @ErrorPlace}. Assigning a value to this
+         * constant is mandatory if using {@link DefaultPlaceManager}. As a convenience, you may have your custom {@link
+         * PlaceManager} reuse this constant.
+         */
+        public Builder errorPlace(String errorPlace) {
+            this.errorPlace = errorPlace;
+            return this;
+        }
+
+        /**
+         * Bind a value to the constant annotated with {@link UnauthorizedPlace @UnauthorizedPlace}. Assigning a value
+         * to this constant is mandatory if using {@link DefaultPlaceManager}. As a convenience, you may have your
+         * custom {@link PlaceManager} reuse this constant.
+         */
+        public Builder unauthorizedPlace(String unauthorizedPlace) {
+            this.unauthorizedPlace = unauthorizedPlace;
+            return this;
+        }
+
         public DefaultModule build() {
             return new DefaultModule(this);
         }
+
+        Class<? extends TokenFormatter> getTokenFormatterClass() {
+            return tokenFormatterClass;
+        }
+
+        Class<? extends PlaceManager> getPlaceManagerClass() {
+            return placeManagerClass;
+        }
+
+        Class<? extends EventBus> getEventBusClass() {
+            return eventBusClass;
+        }
+
+        String getUnauthorizedPlace() {
+            return unauthorizedPlace;
+        }
+
+        String getErrorPlace() {
+            return errorPlace;
+        }
+
+        String getDefaultPlace() {
+            return defaultPlace;
+        }
     }
 
+    private final Builder builder;
+
     /**
-     * When instantiating the module this way be sure to read
-     * {@link DefaultPlaceManager}
-     *
-     * <b>Important!</b> If you use this class, don't forget to bind
-     * {@link com.gwtplatform.mvp.client.annotations.DefaultPlace DefaultPlace},
-     * {@link com.gwtplatform.mvp.client.annotations.ErrorPlace ErrorPlace} and
-     * {@link com.gwtplatform.mvp.client.annotations.UnauthorizedPlace
-     * UnauthorizedPlace} to Presenter name tokens in your Gin module.<br/>
-     *
-     * @see <a href="https://github.com/ArcBees/GWTP/wiki/PlaceManager">See
-     *      PlaceManager wiki for more examples</a>
+     * When instantiating the module this way be sure to read {@link DefaultPlaceManager} and manually bind the required
+     * constants. Consider using {@link DefaultModule.Builder} if you need to use a different implementation than the
+     * default one for a binding.
+     * <p/>
+     * <b>Important!</b> If you use this class, don't forget to bind {@link DefaultPlace}, {@link ErrorPlace} and {@link
+     * UnauthorizedPlace} to name tokens associated to a presenter.<br/>
      */
     public DefaultModule() {
         this(new Builder());
     }
 
-    /**
-     * Manually setup a PlaceManager. See {@link DefaultPlaceManager} for more
-     * details.<br/>
-     *
-     * @param placeManagerClass
-     *            {@link DefaultPlaceManager} @see <a
-     *            href="https://github.com/ArcBees/GWTP/wiki/PlaceManager">See
-     *            PlaceManager wiki for more examples</a>
-     *
-     * @deprecated Please use the {@link com.gwtplatform.mvp.client.gin.DefaultModule.Builder}.
-     */
-    @Deprecated
-    public DefaultModule(Class<? extends PlaceManager> placeManagerClass) {
-        this(placeManagerClass, ParameterTokenFormatter.class);
-    }
-
-    /**
-     * Manually setup a {@link PlaceManager} and {@link TokenFormatter}.
-     *
-     * <p>
-     * See {@link DefaultPlaceManager}, {@link ParameterTokenFormatter} and {@link RouteTokenFormatter} for more
-     * details.
-     * </p>
-     *
-     * @param placeManagerClass   The {@link PlaceManager} implementation.
-     * @param tokenFormatterClass The {@link TokenFormatter} implementation.
-     *
-     * @deprecated Please use the {@link com.gwtplatform.mvp.client.gin.DefaultModule.Builder}.
-     */
-    @Deprecated
-    public DefaultModule(Class<? extends PlaceManager> placeManagerClass,
-                         Class<? extends TokenFormatter> tokenFormatterClass) {
-        this.placeManagerClass = placeManagerClass;
-        this.tokenFormatterClass = tokenFormatterClass;
-    }
-
     private DefaultModule(Builder builder) {
-        this.placeManagerClass = builder.placeManagerClass;
-        this.tokenFormatterClass = builder.tokenFormatterClass;
+        this.builder = builder;
     }
 
     @Override
     protected void configure() {
         install(new CommonGinModule());
 
-        bind(EventBus.class).to(SimpleEventBus.class).in(Singleton.class);
-        bind(TokenFormatter.class).to(tokenFormatterClass).in(Singleton.class);
         bind(RootPresenter.class).asEagerSingleton();
+
+        bind(EventBus.class).to(builder.getEventBusClass()).in(Singleton.class);
+        bind(TokenFormatter.class).to(builder.getTokenFormatterClass()).in(Singleton.class);
+        bind(PlaceManager.class).to(builder.getPlaceManagerClass()).in(Singleton.class);
         bind(GoogleAnalytics.class).to(GoogleAnalyticsImpl.class).in(Singleton.class);
-        bind(PlaceManager.class).to(placeManagerClass).in(Singleton.class);
+
+        maybeBindConstant(DefaultPlace.class, builder.getDefaultPlace());
+        maybeBindConstant(ErrorPlace.class, builder.getErrorPlace());
+        maybeBindConstant(UnauthorizedPlace.class, builder.getUnauthorizedPlace());
+    }
+
+    private void maybeBindConstant(Class<? extends Annotation> annotationClass, String nameToken) {
+        if (nameToken != null && !nameToken.isEmpty()) {
+            bindConstant().annotatedWith(annotationClass).to(nameToken);
+        }
     }
 }
